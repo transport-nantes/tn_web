21791fa23eb6baceef2b0e58a066823efa7c03c0
Jeff Abrahamson <jeff@p27.eu>
2020-02-12 00:10:48 +0100

    Add nascent django projetc

---
+++ README.md
@@ -0,0 +1,1 @@
+This repository contains the Transport Nantes website code.
---
+++ transport_nantes/transport_nantes/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for transport_nantes project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.0/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'transport_nantes.settings')
+
+application = get_wsgi_application()
