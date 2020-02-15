854cd33e073ba2c8f9daa8d8a2c70f8e014b9d8f
Jeff Abrahamson <jeff@p27.eu>
2020-02-15 13:19:08 -0700

    Enable admin interface for surveys

--- transport_nantes/surveys/admin.py
+++ transport_nantes/surveys/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Survey, SurveyQuestion, SurveyResponder, SurveyResponse
+
+admin.site.register(Survey)
+admin.site.register(SurveyQuestion)
+admin.site.register(SurveyResponder)
+admin.site.register(SurveyResponse)
