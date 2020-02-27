d475ede23eacdd05228d62714420b7b0639d759a
Jeff Abrahamson <jeff@p27.eu>
2020-02-27 11:08:58 -0700

    Add Grand Nantes and Velopolitain apps

---
+++ transport_nantes/grand_nantes/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class GrandNantesConfig(AppConfig):
+    name = 'grand_nantes'
---
+++ transport_nantes/velopolitain/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class VelopolitainConfig(AppConfig):
+    name = 'velopolitain'
