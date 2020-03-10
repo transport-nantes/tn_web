b34102f0ea3582c0809546cf5e0d3c31c0c2c207
Jeff Abrahamson <jeff@p27.eu>
2020-03-10 15:25:54 +0100

    Re-add staticfiles app, even for production

--- transport_nantes/transport_nantes/settings.py
+++ transport_nantes/transport_nantes/settings.py
@@ -36,6 +36,7 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
+    'django.contrib.staticfiles',
 ] + settings_local.MORE_INSTALLED_APPS
 
 MIDDLEWARE = [
