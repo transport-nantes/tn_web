77f45ca766d2a94d99ddd2ccd7a0c6a234fdcc4d
Jeff Abrahamson <jeff@p27.eu>
2020-05-07 11:18:06 +0200

    Add basic text for observatoire du velopolitain
    Part of #27.

--- transport_nantes/velopolitain_observatoire/views.py
+++ transport_nantes/velopolitain_observatoire/views.py
@@ -1,9 +1,16 @@
 from django.views.generic.base import TemplateView
 
 # Create your views here.
+
 class MainVelopolitainObservatoire(TemplateView):
-    template_name = 'velopolitain_observatoire/index.html'
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
+        self.template_name = 'velopolitain_observatoire/{sl}.html'.format(sl=context['slug'])
         return context
+
+class VelopolitainObservatoireCountApp(TemplateView):
+    template_name = 'velopolitain_observatoire/count_app.html'
+
+class VelopolitainObservatoireCountForm(TemplateView):
+    template_name = 'velopolitain_observatoire/count_form.html'
