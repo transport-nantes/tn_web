b5479327a70f01a967c6e984a21cda065c91732d
Jeff Abrahamson <jeff@p27.eu>
2020-05-09 15:50:33 +0200

    Implement role context processor
    I sometimes want to have features active or inactive depending upon
    role.  This will let me do that.
    
    Part of #16.

---
+++ transport_nantes/asso_tn/context_processors.py
@@ -0,0 +1,27 @@
+"""
+Some request processors that return dictionaries to be merged into a
+template context. Each function takes the request object as its only
+parameter and returns a dictionary to add to the context.
+
+These are referenced from the 'context_processors' option of the
+configuration of a DjangoTemplates backend and used by RequestContext.
+
+"""
+
+from django.conf import settings
+
+def role(request):
+    """
+    The settings_local.py should define a role.  Make sure it's
+    provided in the context, as well as a boolean for whether or not
+    we're running in production.
+    """
+    if hasattr(settings, 'ROLE'):
+        role = settings.ROLE
+    else:
+        role = 'unknown'
+    if 'production' == role:
+        is_prod = True
+    else:
+        is_prod = False
+    return {'role': role, 'is_production': is_prod}
