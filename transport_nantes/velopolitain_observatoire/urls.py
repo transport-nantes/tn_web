d7d2918aca10947bfd7749b3783641292d8c3d79
Jeff Abrahamson <jeff@p27.eu>
2020-06-19 15:59:45 +0200

    Advertise OV
    Part of #46.

--- transport_nantes/velopolitain_observatoire/urls.py
+++ transport_nantes/velopolitain_observatoire/urls.py
@@ -4,7 +4,7 @@ from . import views
 
 app_name = 'velopolitain_observatoire'
 urlpatterns = [
-    #path('', views.MainVelopolitainObservatoire.as_view(), name='vobs_index'),
+    path('', views.MainVelopolitainObservatoire.as_view(), name='vobs_index'),
     path('b/<str:slug>', views.MainVelopolitainObservatoire.as_view(), name='blog'),
     path('observatoire', views.VelopolitainObservatoireCountApp.as_view(), name='count_app'),
     path('rattrapage', views.VelopolitainObservatoireCountForm.as_view(), name='count_form'),
