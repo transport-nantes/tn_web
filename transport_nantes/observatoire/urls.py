038a08e6c83e8b4451d686e87e39dd887b968cf5
Jeff Abrahamson <jeff@p27.eu>
2020-07-01 21:40:55 +0200

    Add mode d'emploi and beginnings of progress page
    Both are drafts.
    
    Part of #48.

--- transport_nantes/observatoire/urls.py
+++ transport_nantes/observatoire/urls.py
@@ -6,4 +6,10 @@ app_name = 'observatoire'
 urlpatterns = [
     path('<int:observatoire_id>',
          views.MainObservatoire.as_view(), name='index'),
+    path('progress/<int:observatoire_id>',
+         views.ProgressObservatoire.as_view(), name='progress'),
+    path('progress/<int:observatoire_id>/<int:person_id>',
+         views.ProgressObservatoire.as_view(), name='progress'),
+    path('b/<int:observatoire_id>/<str:slug>',
+         views.BlogObservatoire.as_view(), name='blog'),
 ]
--- transport_nantes/observatoire/views.py
+++ transport_nantes/observatoire/views.py
@@ -1,6 +1,6 @@
 from django.views.generic.base import TemplateView
 # from django.shortcuts import render
-from .models import Observatoire
+from .models import Observatoire, ObservatoirePerson
 import datetime
 
 class MainObservatoire(TemplateView):
@@ -15,4 +15,33 @@ class MainObservatoire(TemplateView):
         context['day_offset'] = (datetime.date.today() - observatoire.start_date).days
         print(context)
         print(type(context['day_offset']))
+        return context
+
+class ProgressObservatoire(TemplateView):
+    template_name = 'observatoire/progress.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        print(kwargs)
+        observatoire = Observatoire.objects.filter(
+            id=kwargs['observatoire_id'])[0]
+        context['day_offset'] = (datetime.date.today() - observatoire.start_date).days
+        context['observatoire'] = observatoire
+        if 'person_id' in kwargs:
+            context['person'] = ObservatoirePerson.objects.filter(
+                id=kwargs['person_id'])
+        persons = ObservatoirePerson.objects.filter(observatoire_id=kwargs['observatoire_id'])
+        print(context)
+        print(type(context['day_offset']))
+        return context
+
+class BlogObservatoire(TemplateView):
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        self.template_name = 'observatoire/{sl}.html'.format(sl=context['slug'])
+        observatoire = Observatoire.objects.filter(
+            id=kwargs['observatoire_id'])[0]
+        context['observatoire'] = observatoire
+        context['day_offset'] = (datetime.date.today() - observatoire.start_date).days
         return context
