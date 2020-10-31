e6869b8331b872f919667117bdea71713aff8b64
Jeff Abrahamson <jeff@p27.eu>
2020-10-31 21:07:35 +0100

    Clean up login form
    Re-add password fields.
    
    Part of #51.

--- transport_nantes/authentication/forms.py
+++ transport_nantes/authentication/forms.py
@@ -7,25 +7,11 @@ class SignUpForm(UserCreationForm):
     email = forms.EmailField(max_length=254,
                              label="Adresse mél", help_text='Obligatoire')
     password1 = forms.CharField(widget=forms.PasswordInput, max_length=254,
-                                label="mot de passe", required=False,
-                                help_text="Facultatif.  Choisissez un bon : au moins 8 caractères, etc.")
+                                label="Mot de passe", required=False,
+                                help_text="Facultatif")
     password2 = forms.CharField(widget=forms.PasswordInput, max_length=254,
-                                label="mot de passe", required=False,
+                                label="Mot de passe", required=False,
                                 help_text="Encore la même chose")
-
-    class Meta:
-        model = User
-        fields = ('email', 'password1', 'password2', )
-
-class LoginForm(AuthenticationForm):
-    email = forms.EmailField(max_length=254,
-                             label="Adresse mél", help_text='Obligatoire')
-    password1 = forms.CharField(widget=forms.PasswordInput, max_length=254,
-                                label="mot de passe", required=False,
-                                help_text="Facultatif, laissez blanc afin de recevoir un mél avec un lien magique.")
-    password2 = forms.CharField(widget=forms.PasswordInput, max_length=254,
-                                label="mot de passe", required=False,
-                                help_text="Encore la même chose.")
 
     class Meta:
         model = User
