2e9724000a6dc25de71bfb17027f7565828728b5
Jeff Abrahamson <jeff@p27.eu>
2020-11-23 18:51:27 +0100

    Fix click + collect sign-up
    The reserved (confirmation) page was independently accessible.  We
    want to make that only happen if we are debugging.
    
    Also, add commune to information requested.
    
    Part of #64.

--- transport_nantes/clickcollect/forms.py
+++ transport_nantes/clickcollect/forms.py
@@ -4,22 +4,30 @@ from django.forms import ModelForm
 from captcha.fields import CaptchaField
 
 class ClickCollectForm(ModelForm):
-    first_name = forms.CharField(
-        max_length=80,
-        label="Prénom", help_text='Obligatoire')
-    last_name = forms.CharField(
-        max_length=80,
-        label="Nom", help_text='Obligatoire')
-    email = forms.EmailField(
-        max_length=254,
-        label="Adresse mél", help_text='Obligatoire')
     captcha = CaptchaField(
         label="Je suis humain",
         help_text="Obligatoire : disponibilité réservée aux humains",
         error_messages=dict(invalid="captcha incorrect, veuillez réessayer"))
+    commune = forms.CharField(
+        max_length=100,
+        label="Commune", help_text='Nous aide à vous apporter des actualités')
+    code_postal = forms.CharField(
+        max_length=80,
+        label="Code postal", help_text='Obligatoire')
 
     class Meta:
         model = User
-        fields = ('first_name', 'last_name', 'email',
-                  'captcha',)
+        fields = ('first_name', 'last_name', 'email', 'captcha',
+                  'commune', 'code_postal')
+
+        labels = {
+            'first_name': "Prénom",
+            'last_name': "Nom",
+            'email': "Adresse mél",
+        }
+        help_texts = {
+            'first_name': "Obligatoire",
+            'last_name': "Obligatoire",
+            'email': "Obligatoire",
+        }
 
--- transport_nantes/clickcollect/views.py
+++ transport_nantes/clickcollect/views.py
@@ -1,7 +1,8 @@
 from django.contrib.auth.models import User
 from django.views.generic.base import TemplateView
 from django.views.generic.edit import FormView
-from django.urls import reverse
+from django.shortcuts import render
+# from django.urls import reverse
 from django.core.exceptions import ObjectDoesNotExist
 from django.utils.crypto import get_random_string
 from .forms import ClickCollectForm
@@ -12,32 +13,25 @@ from .models import ClickableCollectable, ClickAndCollect
 class GiletReserveView(FormView):
     template_name = 'clickcollect/reserve.html'
     form_class = ClickCollectForm
-    #success_url = reverse('click_collect:gilet_reserved')
-    success_url = "gr2"         # Why didn't this work with reverse?
+    # success_url = reverse_lazy('click_collect:gilet_reserved')
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context['hero'] = True
         context['hero_image'] = 'velopolitain/gilet-banner.png'
         context['hero_title'] = 'Les rencontres visibles'
-        self.success_url = reverse('click_collect:gilet_reserved')
-        context['success_url'] = reverse('click_collect:gilet_reserved')
         return context
 
     def form_valid(self, form):
         # This method is called when valid form data has been POSTed.
         # It should return an HttpResponse.
         #form.send_email()
-        print("Form is valid")
-        # print(reverse('click_collect:gilet_reserved'))
         user = form.save(commit=False)
         try:
             user.refresh_from_db()
         except ObjectDoesNotExist:
             print('ObjectDoesNotExist')
-            print((user, user.id, user.pk,))
             pass            # I'm not sure this can ever happen.
-        print((user, user.pk,))
         if user is None or user.pk is None:
             user = User.objects.filter(email=form.cleaned_data['email']).first()
             if user is None:
@@ -46,15 +40,30 @@ class GiletReserveView(FormView):
                 user.first_name = form.cleaned_data['first_name']
                 user.last_name = form.cleaned_data['last_name']
                 user.email = form.cleaned_data['email']
-                user.save()
-        print((user, user.pk,))
+        user.profile.commune = form.cleaned_data['commune']
+        user.profile.code_postal = form.cleaned_data['code_postal']
+        user.save()
+        user.profile.save()
         gilet = ClickableCollectable.objects.get(collectable_token='gilet-transport-nantes')
         click_and_collect = ClickAndCollect.objects.create(user=user, collectable=gilet)
         click_and_collect.save()
-        print((click_and_collect, click_and_collect.pk,))
-        return super(GiletReserveView, self).form_valid(form)
+        # return super(GiletReserveView, self).form_valid(form)
+        return render(self.request, 'clickcollect/reserved.html',
+                      {
+                          'hero': True,
+                          'hero_image': 'velopolitain/gilet-banner.png',
+                          'hero_title': 'Les rencontres visibles',
+                      }
+        )
 
 class GiletReservedView(TemplateView):
+    """View class only used whilst debugging.
+
+    This is useful for revealing the thank you page, but under normal
+    circumstances it should not be revealed without first filling out
+    the form.
+
+    """
     template_name = 'clickcollect/reserved.html'
 
     def get_context_data(self, **kwargs):
@@ -62,6 +71,5 @@ class GiletReservedView(TemplateView):
         context['hero'] = True
         context['hero_image'] = 'velopolitain/gilet-banner.png'
         context['hero_title'] = 'Les rencontres visibles'
-        # Argument passed is the slug.  This should become a db lookup instead.
-        # For the moment, all slugs lead to the laboratoire.
+        context['hero_description'] = '(currently debugging)'
         return context
