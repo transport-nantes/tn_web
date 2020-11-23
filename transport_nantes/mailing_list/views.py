2983622ac6b59f01aff5620ca322e28b239a9763
Jeff Abrahamson <jeff@p27.eu>
2020-11-23 18:50:24 +0100

    Clean up code
    Remove some print statements, comment out some unneeded includes.
    
    Part of #63.

--- transport_nantes/mailing_list/views.py
+++ transport_nantes/mailing_list/views.py
@@ -3,8 +3,9 @@ from django.core.exceptions import ObjectDoesNotExist
 from django.shortcuts import render
 from django.views.generic.base import TemplateView
 from django.views.generic.edit import FormView
-from django.urls import reverse
-from django.urls import reverse_lazy
+# from django.urls import reverse
+# from django.urls import reverse_lazy
+from django.utils.crypto import get_random_string
 from .forms import MailingListSignupForm
 from .models import MailingList, MailingListEvent
 
@@ -17,7 +18,6 @@ class MailingListSignup(FormView):
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        print('MailingListSignup')
         context['hero'] = True
         context['hero_image'] = 'asso_tn/images-libres/black-and-white-bridge-children-194009-1000.jpg'
         context['hero_title'] = 'Newsletter'
@@ -28,16 +28,12 @@ class MailingListSignup(FormView):
         # It should return an HttpResponse.
         #form.send_email()
         subscribe = False
-        print("Form is valid")
         user = form.save(commit=False)
-        print((user, user.pk, user.email))
         try:
             user.refresh_from_db()
         except ObjectDoesNotExist:
             print('ObjectDoesNotExist')
-            print((user, user.id, user.pk,))
             pass            # I'm not sure this can ever happen.
-        print((user, user.pk,))
         if user is None or user.pk is None:
             user = User.objects.filter(email=form.cleaned_data['email']).first()
             if user is None:
@@ -50,7 +46,6 @@ class MailingListSignup(FormView):
         user.profile.code_postal = form.cleaned_data['code_postal']
         user.save()
         user.profile.save()
-        print(user, user.profile)
         if form.cleaned_data['monthly']:
             newsletter = MailingList.objects.get(
                 mailing_list_token='general-monthly')
@@ -60,7 +55,6 @@ class MailingListSignup(FormView):
                 event_type=MailingListEvent.EventType.SUBSCRIBE)
             subscription.save()
             subscribe = True
-            print(subscription)
         if form.cleaned_data['quarterly']:
             newsletter = MailingList.objects.get(
                 mailing_list_token='general-quarterly')
@@ -70,7 +64,6 @@ class MailingListSignup(FormView):
                 event_type=MailingListEvent.EventType.SUBSCRIBE)
             subscription.save()
             subscribe = True
-            print(subscription)
         if subscribe:
             return render(
                 self.request, 'mailing_list/merci.html',
@@ -84,6 +77,13 @@ class MailingListSignup(FormView):
         return super(MailingListSignup, self).form_valid(form)
 
 class MailingListMerci(TemplateView):
+    """View class only used whilst debugging.
+
+    This is useful for revealing the thank you page, but under normal
+    circumstances it should not be revealed without first filling out
+    the form.
+
+    """
     template_name = 'mailing_list/merci.html'
 
     def get_context_data(self, **kwargs):
@@ -91,5 +91,6 @@ class MailingListMerci(TemplateView):
         context['hero'] = True
         context['hero_image'] = 'asso_tn/images-libres/black-and-white-bridge-children-194009-1000.jpg'
         context['hero_title'] = 'Newsletter'
+        context['hero_description'] = '(currently debugging)'
         return context
 
