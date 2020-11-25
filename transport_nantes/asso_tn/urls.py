2b7cd73a3daeff8421a0e2b3aaa11b91e7bd5195
Jeff Abrahamson <jeff@p27.eu>
2020-11-25 21:05:12 +0100

    Add join menu item at top of page
    Move less important menu items to page bottom.
    These are further improvements suggested by Jean Robino.
    
    Fix/simplify a couple templates.

--- transport_nantes/asso_tn/urls.py
+++ transport_nantes/asso_tn/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-
+from django.views.generic.base import RedirectView
 from . import views
 
 app_name = 'asso_tn'
@@ -9,11 +9,8 @@ urlpatterns = [
                                         hero_image="asso_tn/happy-folks-1000.jpg",
                                         hero_title="Qui sommes-nous ?"),
          name='qui-sommes-nous'),
-    path('join', views.AssoView.as_view(title="Adhésion",
-                                        template_name='asso_tn/join.html',
-                                        hero_image="asso_tn/happy-folks-1000.jpg",
-                                        hero_title="Envie d'en être ?",
-                                        hero_description="Nous sommes plus fort ensemble"),
+    # path('join', views.JoinUsView.as_view(), name='join'),
+    path('join', RedirectView.as_view(url="https://www.helloasso.com/associations/transport-nantes/adhesions/adhesion-transport-nantes-2019/"),
          name='join'),
     path('contact', views.AssoView.as_view(title="Nous contacter",
                                            template_name='asso_tn/contact.html',
