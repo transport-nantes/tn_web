04648c41ab019eb24da28359bc193dfa183673c6
Jeff Abrahamson <jeff@p27.eu>
2020-12-17 13:55:09 +0100

    Add changes to membership page as per staff requests
    Part of #85.

--- transport_nantes/asso_tn/urls.py
+++ transport_nantes/asso_tn/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from django.views.generic.base import RedirectView
+# from django.views.generic.base import RedirectView
 from . import views
 
 app_name = 'asso_tn'
@@ -13,9 +13,14 @@ urlpatterns = [
         hero_image="asso_tn/happy-folks-1000.jpg",
         hero_title="Qui sommes-nous ?"),
          name='qui-sommes-nous'),
-    # path('join', views.JoinUsView.as_view(), name='join'),
-    path('join', RedirectView.as_view(url="https://www.helloasso.com/associations/transport-nantes/formulaires/"),
-         name='join'),
+    path('join', views.AssoView.as_view(
+        title="Nous soutenir",
+        meta_descr="""<meta name="description" content="Depuis 2 ans, Transport Nantes oeuvre pour une mobilité plus sécurisée, plus fluide et plus vertueuse."/>""",
+        twitter_title = "Soutenez les Mobilitains ! | Transport Nantes - Association Nantaise",
+        twitter_descr = "Depuis 2 ans, Transport Nantes oeuvre pour une mobilité plus sécurisée, plus fluide et plus vertueuse.",
+        template_name='asso_tn/join.html',
+        hero_image="asso_tn/happy-folks-1000.jpg",
+    ), name='join'),
     path('contact', views.AssoView.as_view(
         title="Nous contacter",
         meta_descr="""<meta name="description" content="Depuis 2 ans, Transport Nantes oeuvre pour une mobilité plus sécurisée, plus fluide et plus vertueuse."/>""",
