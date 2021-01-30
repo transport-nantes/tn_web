38b424e27f346182d4bb3bcd745972933fec497f
Jeff Abrahamson <jeff@p27.eu>
2021-01-30 18:02:17 +0100

    Test timed tokens
    Part of #51.

--- transport_nantes/asso_tn/tests.py
+++ transport_nantes/asso_tn/tests.py
@@ -1,3 +1,24 @@
 from django.test import TestCase
+import datetime
+
+from .utils import make_timed_token, token_valid
 
 # Create your tests here.
+
+class TimedTokenTest(TestCase):
+
+    def test_expiry(self):
+        """Test token expiration.
+
+        Test that the token remains valid for the time we expect and
+        not after.
+
+        """
+        USER_ID = 3
+        EXPIRY_MINUTES = 2
+        EXPIRY_SECONDS = EXPIRY_MINUTES * 60
+        NOW = datetime.datetime.now().timestamp()
+        token = make_timed_token(USER_ID, EXPIRY_MINUTES, NOW)
+        self.assertEqual(token_valid(token, NOW), USER_ID)
+        self.assertEqual(token_valid(token, NOW + EXPIRY_SECONDS - 1), USER_ID)
+        self.assertEqual(token_valid(token, NOW + EXPIRY_SECONDS + 1), -1)
