b0823e6a37684b830990a647dadec2f2801159a0
Jeff Abrahamson <jeff@p27.eu>
2021-01-30 18:02:17 +0100

    Test timed tokens
    Part of #51.

--- transport_nantes/asso_tn/utils.py
+++ transport_nantes/asso_tn/utils.py
@@ -10,7 +10,7 @@ from django.utils.http import base36_to_int, int_to_base36
 from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 
 
-def make_timed_token(user_id, minutes):
+def make_timed_token(user_id, minutes, test_value_now=None):
     """Make a URL-safe timed token that's valid for minutes minutes.
 
     The token is tied to a specific user to avoid frauds where a token
@@ -18,32 +18,41 @@ def make_timed_token(user_id, minutes):
     to validate a user whose email the person doesn't control or by
     simple fishing for user id).
 
+    The test_value_now is for testing and should not normally be set.
+
     """
     secret = settings.SECRET_KEY
     rand_value = secrets.token_urlsafe(20)
-    now = datetime.datetime.now().timestamp()
-    soon = int_to_base36(int(now + 60 * minutes))
-    hmac = salted_hmac(soon, rand_value + str(user_id)).hexdigest()[:20]
+    if test_value_now is not None:
+        now = test_value_now
+    else:
+        now = datetime.datetime.now().timestamp()
+    soon_seconds = int_to_base36(int(now + 60 * minutes))
+    hmac = salted_hmac(soon_seconds, rand_value + str(user_id)).hexdigest()[:20]
     return '{rnd}|{u}|{t}|{h}'.format(
         rnd=rand_value,
         u=user_id,
-        t=soon,
+        t=soon_seconds,
         h=hmac)
 
-
-def token_valid(timed_token):
+def token_valid(timed_token, test_value_now=None):
     """Validate a timed token.
 
     Return the user id for which the token is valid, if the token is
     valid.  Return -1 if the token is not (either because it is
     malformed or because it has expired).
 
+    The test_value_now is for testing and should not normally be set.
+
     """
     the_rand_value, the_user_id, the_soon, the_hmac = timed_token.split('|')
     computed_hmac = salted_hmac(the_soon, the_rand_value + str(the_user_id)).hexdigest()[:20]
     if computed_hmac != the_hmac:
         return -1
-    now = datetime.datetime.now().timestamp()
+    if test_value_now is not None:
+        now = test_value_now
+    else:
+        now = datetime.datetime.now().timestamp()
     if now > base36_to_int(the_soon):
         return -1
     return int(the_user_id)
