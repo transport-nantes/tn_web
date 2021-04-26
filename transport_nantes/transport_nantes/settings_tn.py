eef0d5970ffca6390f38f4c6c0e2614215c4a841
Jeff Abrahamson <jeff@p27.eu>
2021-04-26 08:47:52 +0200

    Remove old settings and urls
    Part of #121.

--- transport_nantes/transport_nantes/settings_tn.py
+++
@@ -1,8 +0,0 @@
-# Settings file for transport-nantes.com.
-
-from transport_nantes.settings import *
-
-# This is the application object that django's built-in server (i.e.,
-# runserver) will use.  In production, this is specified to gunicorn
-# on the commandline.
-WSGI_APPLICATION = 'transport_nantes.wsgi_tn.application'
