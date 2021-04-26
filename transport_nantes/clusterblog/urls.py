61009d3f302703f99424176388f9226b630aaddf
Jeff Abrahamson <jeff@p27.eu>
2021-04-26 08:48:37 +0200

    Remove ClusterBlog models
    Once this has been applied on beta and production, we can remove the
    app completely.
    
    Part of #121.

--- transport_nantes/clusterblog/urls.py
+++ transport_nantes/clusterblog/urls.py
@@ -4,8 +4,8 @@ from . import views
 
 app_name = 'cluster_blog'
 urlpatterns = [
-    path('e/<str:slug>', views.MainClusterBlog.as_view(),
-         name='deconfinement'),
-    path('EbyC/<int:cluster>/<int:category>', views.CategoryClusterBlog.as_view(),
-         name='deconfinement_role'),
+    # path('e/<str:slug>', views.MainClusterBlog.as_view(),
+    #      name='deconfinement'),
+    # path('EbyC/<int:cluster>/<int:category>', views.CategoryClusterBlog.as_view(),
+    #      name='deconfinement_role'),
 ]
--- transport_nantes/clusterblog/views.py
+++ transport_nantes/clusterblog/views.py
@@ -1,26 +1,26 @@
-from django.views.generic.base import TemplateView
-from .models import ClusterBlog, ClusterBlogEntry, ClusterBlogCategory
-from django.http import HttpResponseRedirect
-from django.urls import reverse
+# from django.views.generic.base import TemplateView
+# from .models import ClusterBlog, ClusterBlogEntry, ClusterBlogCategory
+# from django.http import HttpResponseRedirect
+# from django.urls import reverse
 
-# Create your views here.
+# # Create your views here.
 
-class MainClusterBlog(TemplateView):
+# class MainClusterBlog(TemplateView):
 
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        blog = ClusterBlog.objects.get(clusterblogentry__slug=context['slug'])
-        self.template_name = blog.template_name
-        context['blog'] = blog
-        context['categories'] = ClusterBlogCategory.objects.all().filter(cluster=blog.pk)
-        context['entry'] = ClusterBlogEntry.objects.get(slug=context['slug'])
-        return context
+#     def get_context_data(self, **kwargs):
+#         context = super().get_context_data(**kwargs)
+#         blog = ClusterBlog.objects.get(clusterblogentry__slug=context['slug'])
+#         self.template_name = blog.template_name
+#         context['blog'] = blog
+#         context['categories'] = ClusterBlogCategory.objects.all().filter(cluster=blog.pk)
+#         context['entry'] = ClusterBlogEntry.objects.get(slug=context['slug'])
+#         return context
 
-class CategoryClusterBlog(TemplateView):
+# class CategoryClusterBlog(TemplateView):
 
-    def get(self, request, *args, **kwargs):
-        cluster_id = kwargs['cluster']
-        category_id = kwargs['category']
-        blog = ClusterBlog.objects.get(id=cluster_id)
-        entry = ClusterBlogEntry.random_entry(cluster_id, category_id)
-        return HttpResponseRedirect(reverse('cluster_blog:deconfinement', args=[entry.slug]))
+#     def get(self, request, *args, **kwargs):
+#         cluster_id = kwargs['cluster']
+#         category_id = kwargs['category']
+#         blog = ClusterBlog.objects.get(id=cluster_id)
+#         entry = ClusterBlogEntry.random_entry(cluster_id, category_id)
+#         return HttpResponseRedirect(reverse('cluster_blog:deconfinement', args=[entry.slug]))
