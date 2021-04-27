761e6f62094f69dab0ec4a59de4a2b4a24fef1e6
Shriukan33 <benja.mourgues@hotmail.fr>
2021-04-27 14:16:02 +0200

    Add main page status code test

--- transport_nantes/topicblog/tests.py
+++ transport_nantes/topicblog/tests.py
@@ -1,3 +1,10 @@
 from django.test import TestCase
 
 # Create your tests here.
+class SimpleTest(TestCase):
+
+    def test_main_page_status_code(self):
+        response = self.client.get("/")
+        return self.assertEqual(response.status_code, 200)
+    
+    
