48915cc201cfb42dc72481cb76c7cd123ff2bc6d
Benjamin_Mourgues <70256364+Shriukan33@users.noreply.github.com>
2021-04-27 17:37:26 +0200

    Fix test on main page's status

--- transport_nantes/topicblog/tests.py
+++ transport_nantes/topicblog/tests.py
@@ -1,10 +1,13 @@
 from django.test import TestCase
+from .models import TopicBlogPage
 
 # Create your tests here.
 class SimpleTest(TestCase):
 
     def test_main_page_status_code(self):
         response = self.client.get("/")
-        return self.assertEqual(response.status_code, 200)
-    
-    
+        self.assertEqual(response.status_code, 404)
+
+        TopicBlogPage.objects.create(title="test", slug="test", topic="index", template="topicblog/2020_index.html")
+        response = self.client.get("/")
+        self.assertEqual(response.status_code, 200)
