e5aac52e0e9bf6b98f3b9ad72e887d9f7a1b8db4
Benjamin Mourgues <benja.mourgues@hotmail.fr>
2021-05-05 14:37:49 +0200

    Registered geoplan to admin panel
    Part of #118

--- transport_nantes/geoplan/admin.py
+++ transport_nantes/geoplan/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import MapPage
 
 # Register your models here.
+class GeoplanPageAdmin(admin.ModelAdmin):
+    pass
+
+admin.site.register(MapPage, GeoplanPageAdmin)
