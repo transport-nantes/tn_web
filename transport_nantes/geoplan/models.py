fd3771251945ec3e97c4d8e6e0f74dd6d444b8d6
Benjamin Mourgues <benja.mourgues@hotmail.fr>
2021-05-05 14:35:19 +0200

    Added model for geojson implementation
    Part of #118

--- transport_nantes/geoplan/models.py
+++ transport_nantes/geoplan/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+
+class MapPage(models.Model):
+
+    city = models.CharField(max_length=50)
+    observatory_name = models.CharField(max_length=50)
+    observatory_type = models.CharField(max_length=50)
+    layer_name = models.CharField(max_length=50)
+    layer_position = models.IntegerField()
+    geojson = models.TextField()
+    timestamp = models.DateTimeField(auto_now=False, auto_now_add=False)
+    kilometres = models.FloatField()
