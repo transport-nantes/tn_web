bb41f0118dab957e163b78e8dfae8c17bbdba057
Benjamin Mourgues <benja.mourgues@hotmail.fr>
2021-05-10 21:34:56 +0200

    Added new models for map page
    Part of #118

--- transport_nantes/geoplan/models.py
+++ transport_nantes/geoplan/models.py
@@ -12,3 +12,37 @@ class MapPage(models.Model):
     geojson = models.TextField()
     timestamp = models.DateTimeField(auto_now=False, auto_now_add=False)
     kilometres = models.FloatField()
+
+class MapDefinition(models.Model):
+    """Primary model to display maps according to the city name.
+    city field will get retrieved to create the url leading to the observatory. 
+    /observatoire/<city>/<observatory_type>
+    """
+    # City name : "Nantes"
+    city = models.CharField(max_length=50) 
+    # Public display of what we're looking at : "Plan vélo théorique voté en Juin 2020"
+    observatory_name = models.CharField(max_length=50) 
+    # Sort of observatory, in prevision of future other maps (public transports, bike...)
+    observatory_type = models.SlugField(max_length=100) 
+
+    # Longitude and latitude of the city according to OpenStreetMap 
+    longitude = models.FloatField() 
+    latitude = models.FloatField() 
+
+class MapLayer(models.Model):
+    """Will set parameters for different layers caintained in the MapDefinition"""
+    map_definition = models.ForeignKey(MapDefinition, on_delete=models.CASCADE)
+    layer_name = models.CharField(max_length=100)
+
+    # Depth = position on the stack
+    layer_depth = models.SmallIntegerField() 
+
+class MapContent(models.Model):
+    """Actual content of the layer, displayed on the map"""
+    map_layer = models.ForeignKey(MapLayer, on_delete=models.CASCADE)
+
+    # Raw  geojson file
+    geojson = models.TextField() 
+
+    # Timestamp at creation date, not on edit (we're saving a history)
+    timestamp = models.DateTimeField(auto_now_add=True) 
