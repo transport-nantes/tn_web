5008ea3e18d2ffc703f9c9d079102490fec908db
Benjamin Mourgues <benja.mourgues@hotmail.fr>
2021-05-11 11:27:59 +0200

    Modified the view to fit new models and not saving html document
    Part of #118

--- transport_nantes/geoplan/views.py
+++ transport_nantes/geoplan/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render
 from django.views.generic import TemplateView
+import requests
 
-from .models import MapPage
+from .models import MapPage, MapDefinition, MapLayer, MapContent
 
 import folium
 from pathlib import Path
@@ -12,7 +13,13 @@ class MapView(TemplateView):
     def __init__(self):
         self.build_map()
 
-    def build_map(self, link=None):
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        # Will need to retrieve that from browser probably
+        context["city"] = "Nantes"
+        return context
+
+    def build_map_old(self, link=None):
         """
         Build a map centered on Nantes and adds a layer on it from either local file or link
         """
@@ -32,3 +39,30 @@ class MapView(TemplateView):
         
         template_folder = str(Path(__file__).resolve().parent / 'templates' / 'geoplan')
         m.save(f"{template_folder}/map.html")
+
+    def build_map(self):
+        context = self.get_context_data()
+
+        # The __iexact part makes the search not case senstive (Nantes = nantes)
+        city = MapDefinition.objects.get(city__iexact=context["city"])
+        layer = MapLayer.objects.get(map_definition=city)
+        content = MapContent.objects.get(map_layer=layer)
+
+        # Variables used by folium
+        geojson = content.geojson
+        lat, lon = city.latitude, city.longitude
+        layer_name = layer.layer_name
+
+        # Building the map
+        m = folium.Map(location=[lat, lon], zoom_start=12)
+        # It only loads one geojson file as a layer for now
+        folium.GeoJson(geojson, name=layer_name).add_to(m)
+        # This is the Layer filter to enable / disable datas on map
+        folium.LayerControl(hideSingleBase=True).add_to(m)
+
+        # Produce html code to embed on our page
+        root = m.get_root()
+        html = root.render()
+        context["html_map"] = html.encode('utf8')
+
+        return context
