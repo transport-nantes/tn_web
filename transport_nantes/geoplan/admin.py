7c575249508379f780514705113ee4b23d3230f1
Benjamin Mourgues <benja.mourgues@hotmail.fr>
2021-05-11 11:29:04 +0200

    Added new models to admin panel
    Part of #118

--- transport_nantes/geoplan/admin.py
+++ transport_nantes/geoplan/admin.py
@@ -1,8 +1,11 @@
 from django.contrib import admin
-from .models import MapPage
+from .models import MapPage, MapDefinition, MapLayer, MapContent
 
 # Register your models here.
 class GeoplanPageAdmin(admin.ModelAdmin):
-    prepopulated_fields = {"slug": ("title",)}
+    pass
 
 admin.site.register(MapPage, GeoplanPageAdmin)
+admin.site.register(MapDefinition, GeoplanPageAdmin)
+admin.site.register(MapLayer, GeoplanPageAdmin)
+admin.site.register(MapContent, GeoplanPageAdmin)
