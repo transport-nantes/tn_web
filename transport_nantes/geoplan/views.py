2f62c919a79663ec9948760ae14d1afc3119283c
Benjamin Mourgues <benja.mourgues@hotmail.fr>
2021-05-11 12:35:23 +0200

    Updated View to select and display map according to a city name
    Part of #118

--- transport_nantes/geoplan/views.py
+++ transport_nantes/geoplan/views.py
@@ -10,38 +10,10 @@ from pathlib import Path
 class MapView(TemplateView):
     template_name = "geoplan/base_map.html"
 
-    def __init__(self):
-        self.build_map()
-
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         # Will need to retrieve that from browser probably
         context["city"] = "Nantes"
-        return context
-
-    def build_map_old(self, link=None):
-        """
-        Build a map centered on Nantes and adds a layer on it from either local file or link
-        """
-
-        if link is not None:
-            geojson = str(link)
-        else:
-            plan_nantes = MapPage.objects.get(city='Nantes')
-            field = MapPage._meta.get_field('geojson')
-            geojson = field.value_from_object(plan_nantes)
-
-
-        m = folium.Map(location=[47.2184, -1.5556], zoom_start=12) # Centered on Nantes, can change the map style with the tiles argument
-        folium.GeoJson(geojson, name="Plan vélo théorie").add_to(m) # Adding the first layer of the map with the GeoJSON file
-
-        folium.LayerControl(hideSingleBase=True).add_to(m) # Adds the option to filter Layers
-        
-        template_folder = str(Path(__file__).resolve().parent / 'templates' / 'geoplan')
-        m.save(f"{template_folder}/map.html")
-
-    def build_map(self):
-        context = self.get_context_data()
 
         # The __iexact part makes the search not case senstive (Nantes = nantes)
         city = MapDefinition.objects.get(city__iexact=context["city"])
@@ -63,6 +35,6 @@ class MapView(TemplateView):
         # Produce html code to embed on our page
         root = m.get_root()
         html = root.render()
-        context["html_map"] = html.encode('utf8')
+        context["html_map"] = html
 
         return context
