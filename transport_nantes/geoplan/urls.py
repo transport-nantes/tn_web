8c243ae955806d600ef7d82f700af7bf452480fe
Benjamin Mourgues <benja.mourgues@hotmail.fr>
2021-05-11 16:01:18 +0200

    Added comment to warn about parsing
    Part of #118

--- transport_nantes/geoplan/urls.py
+++ transport_nantes/geoplan/urls.py
@@ -4,5 +4,6 @@ from .views import MapView
 app_name = "geoplan"
 
 urlpatterns = [
+    # This URL is being parsed, city must be an existing city name in database, and observatory name the one indicated in the city object.
     path('<city>/<observatory_name>', MapView.as_view(), name="map")
 ]
