a629351c218b4498abc9fd8db6ba826afa2ce93f
Benjamin Mourgues <benja.mourgues@hotmail.fr>
2021-05-12 16:37:35 +0200

    Added test on geoplan's status codes.
    Part of #118

--- transport_nantes/geoplan/tests.py
+++ transport_nantes/geoplan/tests.py
@@ -1,3 +1,53 @@
+from datetime import datetime
 from django.test import TestCase
 
+
+from .models import MapContent, MapLayer, MapDefinition
+
 # Create your tests here.
+class GeoplanTest(TestCase):
+
+    def setUp(self):
+        self.definition = {
+            "city":"testcity",
+            "observatory_name" :"planvelo",
+            "observatory_type": "test",
+            "longitude": 0.00,
+            "latitude": 0.00
+            }
+
+        # Assignement because an instance is required in Foreign keys
+        map_def = MapDefinition.objects.create(**self.definition)
+
+        self.layer = {
+            "map_definition" : map_def,
+             "layer_name":"test layer",
+             "layer_depth" : 0
+        }
+
+        map_lay = MapLayer.objects.create(**self.layer)
+
+        self.content = {
+            "map_layer": map_lay,
+            "geojson": """{
+                "type": "FeatureCollection",
+                "name": "reseau_velo_metropolitain_4326",
+                "crs": {"type": "name", "properties": {"name": "urn:ogc:def:crs:OGC:1.3:CRS84"}},
+                "features": [
+                    {"type": "Feature", "properties": {"id": 1}, "geometry": {
+                        "type": "MultiLineString", "coordinates": [[ -1.710792615591268, 47.210384214678122 ]]}}
+                ]
+            }""",
+            "timestamp": datetime.now()
+        }
+
+        MapContent.objects.create(**self.content)
+
+    def test_observatoire_status_code(self):
+        # Proper URL scheme
+        response = self.client.get("/observatoire/testcity/planvelo")
+        self.assertEqual(response.status_code, 200)
+
+        # Mistake in URL scheme (city name doesn't exist)
+        response = self.client.get("/observatoire/NOTtestcity/planvelo")
+        self.assertEqual(response.status_code, 404)
