d0984df10206ed4eea84985d5a151cafc2c5647e
Jeff Abrahamson <jeff@p27.eu>
2021-05-16 17:18:47 +0200

    Normalise view filename
    Part of #121.

--- transport_nantes/surveys/urls.py
+++ transport_nantes/surveys/urls.py
@@ -1,34 +1,34 @@
 from django.urls import path
 
-from . import views_m
+from . import views
 
 # We generally call these "questionnaire" now, but originally we
 # called them "survey", at least in English.
 
 app_name = 'surveys'
 urlpatterns = [
-    path('', views_m.MainSurveyView.as_view(), name='survey_root'),
+    path('', views.MainSurveyView.as_view(), name='survey_root'),
     path('questions/<slug:slug>',
-         views_m.QuestionnaireView.as_view(),
+         views.QuestionnaireView.as_view(),
          name='questions'),
 
     # For answering questions.
     path('repondre/1/<slug:slug>',
-         views_m.ResponseView.as_view(),
+         views.ResponseView.as_view(),
          name='response_1'),
 
     # For viewing results.
     path('commune/<int:survey_id>',
-         views_m.CommuneChooserSurveyView.as_view(),
+         views.CommuneChooserSurveyView.as_view(),
          name='choose_commune'),
     path('liste/<int:survey_id>/<int:commune_id>',
-         views_m.ListeChooserSurveyView.as_view(),
+         views.ListeChooserSurveyView.as_view(),
          name='choose_liste'),
     path('question/<int:survey_id>/<int:commune_id>/<int:responder_id>',
-         views_m.QuestionChooserSurveyView.as_view(),
+         views.QuestionChooserSurveyView.as_view(),
          name='choose_question'),
     path('response/<int:survey_id>/<int:commune_id>/<int:responder_id>/<int:question_id>',
-         views_m.ResponseDisplaySurveyView.as_view(),
+         views.ResponseDisplaySurveyView.as_view(),
          name='choose_response'),
     ## Deprecated, was fixed for 
     # path('questionnaire/<int:survey_id>',
