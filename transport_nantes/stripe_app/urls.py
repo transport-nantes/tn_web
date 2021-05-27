0f9afdcf7360ca4f6d7e1a32b8a383dc5cabd383
Shriukan33 <benja.mourgues@hotmail.fr>
2021-05-27 10:08:55 +0200

    Added Webhook to url dispatch
    Also changed format to comply to PEP8 rules
    
    Part of #54

--- transport_nantes/stripe_app/urls.py
+++ transport_nantes/stripe_app/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
-from .views import StripeView, get_public_key, create_checkout_session
+from .views import (
+    StripeView, get_public_key, create_checkout_session, stripe_webhook)
 
 app_name = "stripe_app"
 
@@ -7,6 +8,9 @@ urlpatterns = [
     path('', StripeView.as_view(), name="stripe"),
     path('config/', get_public_key),
     path('create-checkout-session/', create_checkout_session ),
-    path('success/', StripeView.as_view(template_name="stripe_app/success.html")),
-    path('cancelled/', StripeView.as_view(template_name="stripe_app/cancel.html")),
+    path('success/', StripeView.as_view(
+        template_name="stripe_app/success.html")),
+    path('cancelled/', StripeView.as_view(
+        template_name="stripe_app/cancel.html")),
+    path('webhook/', stripe_webhook),
 ]
