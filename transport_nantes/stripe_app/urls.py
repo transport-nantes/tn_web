415c1fefde7bd47c8a44ae5e747f2f8b61cdf388
Shriukan33 <benja.mourgues@hotmail.fr>
2021-06-01 12:19:36 +0200

    Allowed success page to be reverse searched.
    Now use a proper view.
    
    Part of #54

--- transport_nantes/stripe_app/urls.py
+++ transport_nantes/stripe_app/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from .views import (
-    StripeView, get_public_key, create_checkout_session,
+    StripeView, SuccessView, get_public_key, create_checkout_session,
     stripe_webhook, create_payment_intent)
 
 app_name = "stripe_app"
@@ -9,8 +9,7 @@ urlpatterns = [
     path('', StripeView.as_view(), name="stripe"),
     path('config/', get_public_key),
     path('create-checkout-session/', create_checkout_session ),
-    path('success/', StripeView.as_view(
-        template_name="stripe_app/success.html")),
+    path('success/', SuccessView.as_view(), name="stripe_success"),
     path('cancelled/', StripeView.as_view(
         template_name="stripe_app/cancel.html")),
     path('webhook/', stripe_webhook),
