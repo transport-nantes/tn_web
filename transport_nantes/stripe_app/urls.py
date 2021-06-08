2e2db621d99d4f4bca07b1e8a71aeb25586ef86f
Benjamin Mourgues <benja.mourgues@hotmail.fr>
2021-06-08 18:10:47 +0200

    Added names to urls to allow reverse search.
    Part of #54

--- transport_nantes/stripe_app/urls.py
+++ transport_nantes/stripe_app/urls.py
@@ -8,10 +8,10 @@ app_name = "stripe_app"
 urlpatterns = [
     path('', StripeView.as_view(), name="stripe"),
     path('config/', get_public_key),
-    path('create-checkout-session/', create_checkout_session ),
+    path('create-checkout-session/', create_checkout_session, name="checkout-session" ),
     path('success/', SuccessView.as_view(), name="stripe_success"),
     path('cancelled/', StripeView.as_view(
         template_name="stripe_app/cancel.html")),
     path('webhook/', stripe_webhook),
-    path('create-payment-intent/', create_payment_intent),
+    path('create-payment-intent/', create_payment_intent, name="payment-intent"),
 ]
