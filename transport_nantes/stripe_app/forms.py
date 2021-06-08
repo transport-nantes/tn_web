a186b72f41365b27c9eac94d8901bb4e14b7f44f
Benjamin Mourgues <benja.mourgues@hotmail.fr>
2021-06-08 18:09:57 +0200

    Added amount selection to the donation page.
    Part of #54

--- transport_nantes/stripe_app/forms.py
+++ transport_nantes/stripe_app/forms.py
@@ -6,3 +6,11 @@ class DonationForm(forms.Form):
     last_name = forms.CharField(label="Nom")
     address = forms.CharField(label="Adresse")
     postal_code = forms.CharField(label="Code postal", max_length=5)
+    free_amount = forms.IntegerField(label="Montant libre", min_value=1, required=False)
+
+    CHOICES = [ (20, "20 euros"),
+                (35, "35 euros"),
+                (50, "50 euros"),
+                (0, "Montant libre")]
+    amount = forms.ChoiceField(label="Je donne une fois",
+            choices=CHOICES, widget=forms.RadioSelect, initial=0)
