c7030083a3b37d233bec921f959c0a29b608e092
Benjamin Mourgues <benja.mourgues@hotmail.fr>
2021-06-09 17:30:47 +0200

    Created another form to handle amounts given
    This is a first step to create a multi-step form
    with field validation.
    It gives a bit more flexibility to display and
    input validation.
    
    Part of #54

--- transport_nantes/stripe_app/forms.py
+++ transport_nantes/stripe_app/forms.py
@@ -5,12 +5,34 @@ class DonationForm(forms.Form):
     first_name = forms.CharField(label="Prénom")
     last_name = forms.CharField(label="Nom")
     address = forms.CharField(label="Adresse")
-    postal_code = forms.CharField(label="Code postal", max_length=5)
-    free_amount = forms.IntegerField(label="Montant libre", min_value=1, required=False)
+    postal_code = forms.IntegerField(label="Code postal", max_value=99999)
 
-    CHOICES = [ (20, "20 euros"),
-                (35, "35 euros"),
-                (50, "50 euros"),
-                (0, "Montant libre")]
-    amount = forms.ChoiceField(label="Je donne une fois",
-            choices=CHOICES, widget=forms.RadioSelect, initial=0)
+
+class AmountForm(forms.Form):
+    sub_or_donate = [("subscription", "Je donne tous les mois"),
+                     ("payment", "Je donne une fois")]
+    donation_type = forms.ChoiceField(  label="Type de don",
+                                        choices=sub_or_donate,
+                                        widget=forms.RadioSelect,
+                                        initial="subscription")
+
+    CHOICE_PAYMENT = [  (35, "35 euros"),
+                        (55, "55 euros"),
+                        (95, "95 euros"),
+                        (0, "Montant libre")]
+    payment_amount = forms.ChoiceField( label="Paiement unique",
+                                        choices=CHOICE_PAYMENT,
+                                        widget=forms.RadioSelect,
+                                        initial=0)
+
+    CHOICE_SUSBCRIPTION = [ (8, "8 euros"),
+                            (12, "12 euros"),
+                            (20, "20 euros"),
+                            (0, 'Montant libre')]
+    subscription_amount = forms.ChoiceField(label="Paiement mensuel",
+                                            choices=CHOICE_SUSBCRIPTION,
+                                            widget=forms.RadioSelect,
+                                            initial=0)
+
+    free_amount = forms.IntegerField(label="Montant libre",
+                                    min_value=1, required=False)
