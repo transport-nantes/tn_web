fee043306953168038b01437a8072cd4a5bffde1
Benjamin Mourgues <benja.mourgues@hotmail.fr>
2021-06-10 12:06:36 +0200

    Updated form_flow.js to display proper amounts.
    The document now displays different amounts
    depending on if you picked subscription or
    one time payment.
    
    Part of #54

--- transport_nantes/stripe_app/static/stripe_app/form_flow.js
+++ transport_nantes/stripe_app/static/stripe_app/form_flow.js
@@ -7,6 +7,7 @@ $(function(){
 })
 
 // This function inputs a form and returns if it valid or not
+// The listener_id is the id without #. ("id" instead of "#id")
 async function validation_check(listener_id) {
     var valid = await fetch('form-validation/', {
         body: new FormData(document.getElementById(listener_id)),
@@ -29,11 +30,6 @@ async function validation_check(listener_id) {
     })
     return valid
 }
-// function callValidation(listener_id) {await validation_check(listener_id)} 
-
-document.querySelector("#donation_form").addEventListener("submit", () => {
-    console.log("Submitted !")
-})
 
 // Button validating personnal informations step.
 document.querySelector("#toStep2").addEventListener("click", async() => {
@@ -46,9 +42,39 @@ document.querySelector("#toStep2").addEventListener("click", async() => {
     }
     
 })
-
+// This button allows you to go back to the first step
 document.querySelector("#toStep1").addEventListener("click", () => {
     console.log("Return Button clicked !")
     document.getElementById("donation_form").style.display = "block"
     document.getElementById("amount_form").style.display = "none"
+})
+
+// This function will check what set of amount to display
+// depending on what option is selected.
+function donation_selected() {
+    var donation_radios = document.getElementsByName("donation_type")
+    let selected
+    for (const radio_button of donation_radios) {
+        if (radio_button.checked) {
+            selected = radio_button.value
+            break
+        }
+    }
+    return selected
+}
+
+// Adds an event listener to each html object with name="donation_type"
+// The value is then evaluated to display appropriate amounts.
+document.getElementsByName("donation_type").forEach(item => {
+    item.addEventListener('click', event => {
+        let selected = donation_selected()
+        document.getElementById("free_amount").style.display = "block"
+        if (selected == "payment") {
+            document.getElementById("subscription_amount_rb").style.display = "none"
+            document.getElementById("payment_amount_rb").style.display = "block"
+        } else {
+            document.getElementById("subscription_amount_rb").style.display = "block"
+            document.getElementById("payment_amount_rb").style.display = "none"
+        }
+    })
 })
