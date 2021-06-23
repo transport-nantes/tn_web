0ac75d131820bf105ac63eb74ffe0563ddb2627d
Benjamin Mourgues <benjamin@mobilitains.fr>
2021-06-23 15:01:46 +0200

    Add script to change color of selected radio buttons.
    Couldn't find a way to do it with bootstrap.
    
    Part of #54

---
+++ transport_nantes/stripe_app/static/stripe_app/form_style.js
@@ -0,0 +1,24 @@
+
+/* 
+This places an even listener on each radio_button
+Everytime a radio is pressed on the page, the script
+will check for each button if it's checked.
+If checked, background changes. 
+*/
+document.querySelectorAll("input[type='radio']").forEach((item, index) => {
+    item.addEventListener("click", event => {
+        update_style()
+    })
+})
+
+radio_buttons = document.querySelectorAll("input[type='radio']")
+
+function update_style() {
+    radio_buttons.forEach(item => {
+        if (item.checked == true) {
+            item.parentNode.style = "background-color: var(--mobilitains-bleu)"
+        } else {
+            item.parentNode.style = "background-color: white"
+        }
+    })
+}
