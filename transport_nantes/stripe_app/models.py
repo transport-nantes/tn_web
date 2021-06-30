11875df1aa888360c4d8ffe48c51566768362513
Benjamin Mourgues <benjamin@mobilitains.fr>
2021-06-30 13:39:37 +0200

    Created a model to save and track form's completition.
    Step 1 represents the amount form.
    Step 2 represents the personnal informations form
    timestamp is autocompleted by the server's clock.
    
    Part of #54

--- transport_nantes/stripe_app/models.py
+++ transport_nantes/stripe_app/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+class TrackingProgression(models.Model):
+    step_1 = models.BooleanField()
+    step_2 = models.BooleanField()
+    timestamp =models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        if self.step_2 is True:
+            name  = "SUCCESS "
+        else:
+            name = "ABANDON "
+        return name +  str(self.timestamp)[:19]
