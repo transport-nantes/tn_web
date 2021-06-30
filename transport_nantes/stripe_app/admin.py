c012b67a0d07feb0a62e186c4d9b91264051005d
Benjamin Mourgues <benjamin@mobilitains.fr>
2021-06-30 13:40:01 +0200

    Add form completition tracking to admin panel.
    Fields are set to be read-only.
    Beingf read allows us to see the field, although we
    can't edit them.
    The last point is on purpose, we shouldn't be able to alter
    the saved progress of a user.
    
    Part of #54

--- transport_nantes/stripe_app/admin.py
+++ transport_nantes/stripe_app/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import TrackingProgression
 
 # Register your models here.
+class TrackingProgressionAdmin(admin.ModelAdmin):
+    readonly_fields = ("timestamp", "step_1", "step_2", )
+
+admin.site.register(TrackingProgression, TrackingProgressionAdmin)
