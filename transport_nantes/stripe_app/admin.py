0db87eebdf898d3cbace1f482241248f4b29fc72
Benjamin Mourgues <benjamin@mobilitains.fr>
2021-07-06 10:49:40 +0200

    Create Donator and Donation models.
    Donator model is used to store the information about the donator.
    Donation model is used to store the information about the donation.
    
    Each donator can have multiple donations.
    Each donation is a single transaction, linked to a donator.
    
    Add donator and donation records to the admin panel.
    
    Part of #54

--- transport_nantes/stripe_app/admin.py
+++ transport_nantes/stripe_app/admin.py
@@ -1,8 +1,16 @@
 from django.contrib import admin
-from .models import TrackingProgression
+from .models import TrackingProgression, Donation, Donator
 
 # Register your models here.
 class TrackingProgressionAdmin(admin.ModelAdmin):
     readonly_fields = ("timestamp", "step_1", "step_2", )
 
+class DonatorAdmin(admin.ModelAdmin):
+    pass
+
+class DonationAdmin(admin.ModelAdmin):
+    pass
+
 admin.site.register(TrackingProgression, TrackingProgressionAdmin)
+admin.site.register(Donator, DonatorAdmin)
+admin.site.register(Donation, DonationAdmin)
--- transport_nantes/stripe_app/models.py
+++ transport_nantes/stripe_app/models.py
@@ -12,3 +12,23 @@ class TrackingProgression(models.Model):
         else:
             name = "ABANDON "
         return name +  str(self.timestamp)[:19]
+
+class Donator(models.Model):
+    email = models.EmailField(primary_key=True)
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    telephone = models.CharField(max_length=13)
+    gender = models.CharField(choices=[
+                                ("M", "Monsieur"),
+                                ("MME", "Madame")], max_length=3)
+    address = models.CharField(max_length=50)
+    more_adress = models.CharField(blank=True, max_length=50)
+    postal_code = models.IntegerField()
+    city = models.CharField(max_length=50)
+    country = models.CharField(max_length=50)
+
+class Donation(models.Model):
+    donator = models.ForeignKey(Donator, on_delete=models.CASCADE)
+    mode = models.CharField(
+        choices=[("SUB", "Subscription"), ("PAY", "One time donation")], max_length=50)
+    amount = models.FloatField()
