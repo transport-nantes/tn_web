41afec432d3f531f15326acbb1d46f1b39ecd6cd
Benjamin Mourgues <benjamin@mobilitains.fr>
2021-07-27 14:39:38 +0200

    Make read-only fields in donation model.
    This allows the display of timestamp field.
    Also, those information shouldn't be editable.
    
    Part of #54

--- transport_nantes/stripe_app/admin.py
+++ transport_nantes/stripe_app/admin.py
@@ -1,15 +1,18 @@
 from django.contrib import admin
 from .models import TrackingProgression, Donation, Donator
 
-# Register your models here.
+
 class TrackingProgressionAdmin(admin.ModelAdmin):
     readonly_fields = ("timestamp", "step_1", "step_2", )
+
 
 class DonatorAdmin(admin.ModelAdmin):
     pass
 
+
 class DonationAdmin(admin.ModelAdmin):
-    pass
+    readonly_fields = ("timestamp", "mode", "donator", "amount")
+
 
 admin.site.register(TrackingProgression, TrackingProgressionAdmin)
 admin.site.register(Donator, DonatorAdmin)
