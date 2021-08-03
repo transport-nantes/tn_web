232704f7dd6b578d6f850ddb9d6316231fc2fd95
Benjamin Mourgues <benjamin@mobilitains.fr>
2021-08-03 10:48:51 +0200

    Delete some fields from the donation form.
    Reflects changes made to model.
    
    Also made code flake8 friendly.
    
    Part of #54

--- transport_nantes/stripe_app/forms.py
+++ transport_nantes/stripe_app/forms.py
@@ -6,18 +6,13 @@ from django_countries import widgets, countries
 # crispy forms allows to manage layout of forms as well as styles
 from crispy_forms.helper import FormHelper
 from crispy_forms.layout import (ButtonHolder, HTML, Submit,
-                                Layout, Row, Column, Field, Button)
-
+                                 Layout, Row, Column, Field, Button)
 
 
 class DonationForm(forms.Form):
 
-    title = forms.ChoiceField(label="",
-                              choices=[("M", "M"), ("MME", "MME"), ("MMM", "Autre")],
-                              widget=forms.RadioSelect)
-
     first_name = forms.CharField(label="Prénom",
-                                widget=forms.TextInput(
+                                 widget=forms.TextInput(
                                     attrs={'placeholder': 'Prénom*'}))
 
     last_name = forms.CharField(label="Nom",
@@ -28,28 +23,22 @@ class DonationForm(forms.Form):
                             widget=forms.EmailInput(
                                 attrs={'placeholder': 'Adresse e-mail*'}))
 
-    address = forms.CharField(  label="Adresse",
-                                widget=forms.TextInput(
+    address = forms.CharField(label="Adresse",
+                              widget=forms.TextInput(
                                     attrs={'placeholder': 'Adresse postale*'}))
 
     more_address = forms.CharField(label="Complément d'adresse",
                                    widget=forms.TextInput(
-                                       attrs={'placeholder': "Complément d'adresse"}),
+                                       attrs={'placeholder': "Complément d'adresse"}), # noqa
                                    required=False)
 
-    postal_code = forms.IntegerField(label="Code postal",
-                                    max_value=99999,
-                                    widget=forms.TextInput(
+    postal_code = forms.CharField(label="Code postal",
+                                  widget=forms.TextInput(
                                         attrs={'placeholder': 'Code postal*'}))
 
     city = forms.CharField(label="Ville",
-                            widget=forms.TextInput(
+                           widget=forms.TextInput(
                                 attrs={'placeholder': 'Ville*'}))
-
-    cell_phone = forms.CharField(label="Téléphone",
-                                 widget=forms.TextInput(
-                                     attrs={'placeholder': 'Téléphone'}),
-                                 max_length=50, required=False)
 
     country = forms.ChoiceField(
         widget=widgets.CountrySelectWidget, choices=countries, initial="FR")
@@ -63,24 +52,20 @@ class DonationForm(forms.Form):
         super().__init__(*args, **kwargs)
         self.helper = FormHelper()
         self.helper.form_method = "post"
-        self.helper.form_show_labels= False
+        self.helper.form_show_labels = False
 
         self.helper.layout = Layout(
             ButtonHolder(
                 Button("toStep1", "Revenir à l'étape précédente",
-                    css_id="toStep1",
-                    css_class="btn-outline-info",
-                    style="margin-bottom: 1em"),
+                       css_id="toStep1",
+                       css_class="btn-outline-info",
+                       style="margin-bottom: 1em"),
             ),
-            Field("title", template="stripe_app/inline_radio_custom.html", id="title"),
             Row(
                 Column("first_name"),
                 Column("last_name")
             ),
-            Row(
-                Column("mail"),
-                Column("cell_phone")
-            ),
+            "mail",
             HTML("<br />"),
             "address",
             "more_address",
@@ -91,55 +76,56 @@ class DonationForm(forms.Form):
             "country",
             Field("data_collect", template="stripe_app/checkbox_custom.html"),
             Submit("submit", "Soutenir",
-                    css_id="supportButton",
-                    css_class="btn-success"),
+                   css_id="supportButton",
+                   css_class="btn-success"),
         )
+
 
 class AmountForm(forms.Form):
     sub_or_donate = [("payment", "Je donne une fois"),
                      ("subscription", "Je donne tous les mois")]
-    donation_type = forms.ChoiceField(  label="",
-                                        choices=sub_or_donate,
-                                        widget=forms.RadioSelect)
+    donation_type = forms.ChoiceField(label="",
+                                      choices=sub_or_donate,
+                                      widget=forms.RadioSelect)
 
-    CHOICE_PAYMENT = [  (5, "5 euros"),
-                        (10, "10 euros"),
-                        (25, "25 euros"),
-                        (0, "Montant libre")]
-    payment_amount = forms.ChoiceField( label="",
-                                        choices=CHOICE_PAYMENT,
-                                        widget=forms.RadioSelect)
+    CHOICE_PAYMENT = [(5, "5 euros"),
+                      (10, "10 euros"),
+                      (25, "25 euros"),
+                      (0, "Montant libre")]
+    payment_amount = forms.ChoiceField(label="",
+                                       choices=CHOICE_PAYMENT,
+                                       widget=forms.RadioSelect)
 
-    CHOICE_SUSBCRIPTION = [ ("price_1J0of7ClnCBJWy551iIQ6ydg", "8 euros"),
-                            ("price_1J0ogXClnCBJWy552i9Bs2bg", "12 euros"),
-                            ("price_1J0ohVClnCBJWy55dAJxHjXE", "20 euros")]
+    CHOICE_SUSBCRIPTION = [("price_1J0of7ClnCBJWy551iIQ6ydg", "8 euros"),
+                           ("price_1J0ogXClnCBJWy552i9Bs2bg", "12 euros"),
+                           ("price_1J0ohVClnCBJWy55dAJxHjXE", "20 euros")]
     subscription_amount = forms.ChoiceField(label="",
                                             choices=CHOICE_SUSBCRIPTION,
                                             widget=forms.RadioSelect)
 
     free_amount = forms.IntegerField(label="Montant libre",
-                                    min_value=1, required=False,
-                                    widget=forms.TextInput(
-                                     attrs={'placeholder': 'Montant libre'}),)
+                                     min_value=1, required=False,
+                                     widget=forms.TextInput(
+                                         attrs={'placeholder': 'Montant libre'}),)
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.helper = FormHelper()
         self.helper.form_method = "post"
-        self.helper.form_show_labels= False
+        self.helper.form_show_labels = False
 
         self.helper.layout = Layout(
             Field("donation_type",
-                    template="stripe_app/inline_radio_custom.html"),
+                  template="stripe_app/inline_radio_custom.html"),
             Field("subscription_amount",
-                    template="stripe_app/inline_radio_custom.html",
-                    id="subscription_amount_rb", style="display: none"),
+                  template="stripe_app/inline_radio_custom.html",
+                  id="subscription_amount_rb", style="display: none"),
             Field("payment_amount",
-                    template="stripe_app/inline_radio_custom.html",
-                    id="payment_amount_rb", style="display: none"),
+                  template="stripe_app/inline_radio_custom.html",
+                  id="payment_amount_rb", style="display: none"),
             Field("free_amount", id="free_amount", style="display: none"),
             ButtonHolder(
                 Submit('submit', 'Continuer',
-                        css_class='btn btn-primary', css_id="toStep2")
+                       css_class='btn btn-primary', css_id="toStep2")
             )
         )
