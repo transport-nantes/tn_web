ccb98d23fc41feef355719379c04532987bf4081
Benjamin Mourgues <benjamin@mobilitains.fr>
2021-08-05 16:16:19 +0200

    Add styles to donation page.
    form_styles.js handles styles in a dynamic way.
    
    Part of #54

---
+++ transport_nantes/stripe_app/static/stripe_app/form_style.js
@@ -0,0 +1,27 @@
+// Hides the navbar's content in this page
+nav = document.querySelector(".navbar-nav")
+nav.style = "display: none"
+
+/* 
+This places an even listener on each radio_button
+Everytime a radio is pressed on the page, the script
+will check for each button if it's checked.
+If checked, background changes. 
+*/
+document.querySelectorAll("input[type='radio']").forEach((item, index) => {
+    item.addEventListener("click", event => {
+        update_style()
+    })
+})
+
+radio_buttons = document.querySelectorAll("input[type='radio']")
+
+function update_style() {
+    radio_buttons.forEach(item => {
+        if (item.checked == true) {
+            item.parentNode.style = "background-color: var(--mobilitains-bleu)"
+        } else {
+            item.parentNode.style = "background-color: white"
+        }
+    })
+}
