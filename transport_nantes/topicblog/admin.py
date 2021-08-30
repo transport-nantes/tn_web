4ed8896323879a1cf3a37a2cd1c76ea4b8f31db8
Benjamin Mourgues <benjamin@mobilitains.fr>
2021-08-30 11:55:55 +0200

    Add models to admin panel.
    Part of #115

--- transport_nantes/topicblog/admin.py
+++ transport_nantes/topicblog/admin.py
@@ -1,7 +1,21 @@
 from django.contrib import admin
-from .models import TopicBlogPage
+from .models import TopicBlogPage, TopicBlogTemplate, TopicBlogItem
+
 
 class TopicBlogPageAdmin(admin.ModelAdmin):
     prepopulated_fields = {"slug": ("title",)}
 
+
+class TopicBlogTemplateAdmin(admin.ModelAdmin):
+    pass
+
+
+class TopicBlogItemAdmin(admin.ModelAdmin):
+    # Making it read-only allows admins to see its ID
+    # By default, non modifyable fields are hidden in admin panel
+    readonly_fields = ('pk',)
+
+
 admin.site.register(TopicBlogPage, TopicBlogPageAdmin)
+admin.site.register(TopicBlogTemplate, TopicBlogTemplateAdmin)
+admin.site.register(TopicBlogItem, TopicBlogItemAdmin)
