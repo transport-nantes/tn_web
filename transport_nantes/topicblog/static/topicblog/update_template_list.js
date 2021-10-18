cee4c09808e7df37c539101d4ea5aabc13fa547a
Benjamin Mourgues <benjamin@mobilitains.fr>
2021-10-18 11:59:11 +0200

    Update Ajax call on update_template_list to handle new template.
    Part of #189

--- transport_nantes/topicblog/static/topicblog/update_template_list.js
+++ transport_nantes/topicblog/static/topicblog/update_template_list.js
@@ -1,15 +1,36 @@
-$("#id_item_type").change(function () {
-    var url = $("#edition_form").attr("data-template-update-url");  // get the url of the `template_update_url` view
-    var content_type = $(this).val();  // get the selected content_type from the dropdown
+async function update_template_list() {
+  // get the url of the `template_update_url` view
+  var url = $("#edition_form").attr("data-template-update-url");
+  // get the selected content_type from the dropdown
+  var content_type = $('#id_content_type').val();
 
-    $.ajax({                       // initialize an AJAX request
-      url: url,                    // set the url of the request (= localhost:8000/tb/ajax/update-template-list/)
-      data: {
-        'content_type': content_type       // add the content_type to the GET parameters
-      },
-      success: function (data) {   // `data` is the return of the `update_template_list` view function
-        $("#id_template").html(data);  // replace the contents of the template input with the data that came from the server
-      }
-    });
+  // If content type is empty, we should avoid calls to server. 
+  if (content_type) {
+    // initialize an AJAX request
+    $.ajax({
+        // set the url of the request (= localhost:8000/tb/ajax/update-template-list/)
+        url: url,
+        data: {
+          // add the content_type to the GET parameters
+          'content_type': content_type
+        },
+        // `data` is the return of the `update_template_list` view function
+        success: function (data) {
+          // replace the contents of the template input with the data that came from the server
+          $("#id_template").html(data);
+        }
+        // Awaits for the AJAX call to finish to call the function `select_first_item`
+        // It selects the first item of the dropdown list
+      }).then(() => {select_first_item()});
+  } else {
+    $("#id_template").html("<option value=''>---------</option>");
+  }
+}
 
-  });
+function select_first_item() {
+  $('#id_template option[value="1"]').attr('selected', 'selected');
+}
+
+$("#id_content_type").change(async () => {
+  await update_template_list()
+});
