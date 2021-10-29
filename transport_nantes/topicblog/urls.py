7da8852845bb14a99982de8ad8d599481ced6104
Jeff Abrahamson <jeff@p27.eu>
2021-10-29 08:44:37 +0200

    Activate TBv2 editor in prod

--- transport_nantes/topicblog/urls.py
+++ transport_nantes/topicblog/urls.py
@@ -8,40 +8,30 @@ app_name = 'topic_blog'
 urlpatterns = [
     path('t/<slug:item_slug>/', TopicBlogItemView.as_view(),
          name='view_item_by_slug'),
+
+    path('admin/view/<int:pkid>/', TopicBlogItemViewOne.as_view(),
+         name='view_item_by_pkid_only'),
+    path('admin/view/<int:pkid>/<slug:item_slug>/',
+         TopicBlogItemViewOne.as_view(),
+         name='view_item_by_pkid'),
+
+    path('admin/new/', TopicBlogItemEdit.as_view(),
+         name='new_item'),
+    path('admin/edit/<int:pkid>/', TopicBlogItemEdit.as_view(),
+         name='edit_item_by_pkid'),
+    path('admin/edit/<int:pkid>/<slug:item_slug>/',
+         TopicBlogItemEdit.as_view(),
+         name='edit_item'),
+    path('admin/edit/<slug:item_slug>/', TopicBlogItemEdit.as_view(),
+         name='edit_item_by_slug'),
+
+    path('admin/list/', TopicBlogItemList.as_view(),
+         name='list_items'),
+    path('admin/list/<slug:item_slug>/', TopicBlogItemList.as_view(),
+         name='list_items_by_slug'),
+    path('ajax/update-template-list/', update_template_list,
+         name="update_template_list")
 ]
-if ROLE != 'production':
-    # beta and dev
-    urlpatterns += [
-        path('admin/view/<int:pkid>/',
-             TopicBlogItemViewOne.as_view(),
-             name='view_item_by_pkid_only'),
-        path('admin/view/<int:pkid>/<slug:item_slug>/',
-             TopicBlogItemViewOne.as_view(),
-             name='view_item_by_pkid'),
-
-        path('admin/new/',
-             TopicBlogItemEdit.as_view(),
-             name='new_item'),
-        path('admin/edit/<int:pkid>/',
-             TopicBlogItemEdit.as_view(),
-             name='edit_item_by_pkid'),
-        path('admin/edit/<int:pkid>/<slug:item_slug>/',
-             TopicBlogItemEdit.as_view(),
-             name='edit_item'),
-        path('admin/edit/<slug:item_slug>/',
-             TopicBlogItemEdit.as_view(),
-             name='edit_item_by_slug'),
-
-        path('admin/list/',
-             TopicBlogItemList.as_view(),
-             name='list_items'),
-        path('admin/list/<slug:item_slug>/',
-             TopicBlogItemList.as_view(),
-             name='list_items_by_slug'),
-        path('ajax/update-template-list/',
-             update_template_list,
-             name="update_template_list")
-    ]
 
 # Need topic creation.
 # Need markdown translator.
