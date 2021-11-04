adbb22302a20ffefeb45cc947ff7b6aa5c8f5c0b
Benjamin Mourgues <benjamin@mobilitains.fr>
2021-11-04 11:39:03 +0100

    Add Variant creation
    By creating variants and copies, we want to have some history of a slug,
    and allow ourselves to test different variations of a slug, for example,
    changing social media images and titles.
    
    /!\ Implementation requires changing the settings_local.py file. /!\
    
    Part of #240

--- transport_nantes/topicblog/forms.py
+++ transport_nantes/topicblog/forms.py
@@ -11,13 +11,24 @@ class TopicBlogItemForm(ModelForm):
     class Meta:
         model = TopicBlogItem
         # Admins can still edit those values
-        exclude = ('item_sort_key', 'servable', 'user', 'published')
+        exclude = ('item_sort_key', 'servable', 'user', 'published',
+                   'publication_date')
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         if self.fields["content_type"] is None:
             self.fields['template'] = ModelChoiceField(
                 queryset=TopicBlogTemplate.objects.none())
+        # The two next bool set the availability of buttons in the template
+        # variant available allows the user to create a variant with a button
+        # is editable allows the user to save the changes like a normal form.
+        self.instance: TopicBlogItem
+        self.variant_available = self.instance.can_create_variant()
+        self.is_editable = self.instance.is_editable()
+
+        # When an item is published, its slug becomes frozen (unmodifiable).
+        if self.instance.publication_date:
+            self.fields['slug'].widget.attrs['readonly'] = True
 
         # self.data is the POST data
         if 'template' in self.data:
