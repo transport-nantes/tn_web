4f620fc963494e010a1f637ca23149e1304a6e45
Benjamin Mourgues <benjamin@mobilitains.fr>
2021-12-14 15:58:00 +0100

    Apply PEP8 formatting

--- transport_nantes/authentication/views.py
+++ transport_nantes/authentication/views.py
@@ -7,12 +7,9 @@ from django.contrib.sites.shortcuts import get_current_site
 from django.shortcuts import render, redirect
 from django.http import HttpResponseServerError
 from django.template.loader import render_to_string
-from django.urls import reverse
-from django.utils.encoding import force_bytes, force_text
-from django.utils.http import urlsafe_base64_encode, urlsafe_base64_decode
 from django.utils.crypto import get_random_string
 from django.core.exceptions import ObjectDoesNotExist
-from django.contrib.auth.views import LoginView, LogoutView
+from django.contrib.auth.views import LogoutView
 from django.conf import settings
 
 from authentication.forms import SignUpForm, UserUpdateForm, ProfileUpdateForm
@@ -37,8 +34,10 @@ A quick note on the captcha:
     leaks to third parties, especially to GAFAs.
 
 """
+
+
 def login(request, base_template=None):
-    
+
     if request.method == 'POST':
         form = SignUpForm(request.POST)
         if form.is_valid():
@@ -48,7 +47,9 @@ def login(request, base_template=None):
                 user.refresh_from_db()
                 if user.profile.authenticates_by_mail:
                     send_activation(request, user, False, remember_me)
-                    return render(request, 'authentication/account_activation_sent.html', {'is_new': False})
+                    return render(request,
+                                  'authentication/account_activation_sent.html', # noqa
+                                  {'is_new': False})
             except ObjectDoesNotExist:
                 print('ObjectDoesNotExist')
                 pass            # I'm not sure this can ever happen.
@@ -56,11 +57,12 @@ def login(request, base_template=None):
             # There should be precisely one or zero existing user with the
             # given email, but since the django user model doesn't impose
             # a unique constraint on user emails, I'm stuck verifying this.
-            existing_users = User.objects.filter(email=form.cleaned_data['email'])
+            existing_users = User.objects.filter(
+                email=form.cleaned_data['email'])
             if len(existing_users) > 1:
                 return HttpResponseServerError(
                     "Data error: Multiple email addresses found")
-            
+
             # If password given by user connect with it else connect by mail
             password1 = form.cleaned_data["password1"]
             password2 = form.cleaned_data["password2"]
@@ -68,17 +70,20 @@ def login(request, base_template=None):
                 # If user exists and gives password, connect him
                 if len(existing_users) == 1:
                     existing_user = existing_users[0].username
-                    user = auth.authenticate(username= existing_user, password= password1)
+                    user = auth.authenticate(username=existing_user,
+                                             password=password1)
 
                     if user:
                         auth.login(request, user)
-                        # Set session cookie expiration to session duration if "False" otherwise for 30 days
-                        if remember_me == False:
+                        # Set session cookie expiration to session duration if
+                        # "False" otherwise for 30 days
+                        if not remember_me:
                             request.session.set_expiry(0)
                         else:
                             request.session.set_expiry(60*60*24*30)
                         return redirect("index")
-                # If user doesn't exist and give password, create account and send a confirmation mail
+                # If user doesn't exist and give password, create account
+                # and send a confirmation mail
                 if len(existing_users) == 0:
                     if password2 and password1 == password2:
                         user.email = form.cleaned_data['email']
@@ -87,30 +92,41 @@ def login(request, base_template=None):
                         user.set_password(password1)
                         user.save()
                         send_activation(request, user, True, remember_me)
-                        return render(request, 'authentication/account_activation_sent.html', {'is_new': True})
+                        return render(request,
+                                      'authentication/account_activation_sent.html', # noqa
+                                      {'is_new': True})
                     elif not password2 or password1 != password2:
                         # return to login page with password error message
                         context = {}
                         context["form"] = form
-                        context["pwd_error"] = "Veuillez entrer à nouveau le mot de passe."
-                        return render(request, 'authentication/login.html', context)
-            
+                        context["pwd_error"] = \
+                            "Veuillez entrer à nouveau le mot de passe."
+                        return render(request, 'authentication/login.html',
+                                      context)
+
             # If no password given
             else:
                 # If user exisis, send authentication mail
                 if len(existing_users) == 1:
                     existing_user = existing_users[0]
                     if existing_user.profile.authenticates_by_mail:
-                        send_activation(request, existing_user, False, remember_me)
-                        return render(request, 'authentication/account_activation_sent.html', {'is_new': False})
-                # If user doesn't exist, create account and send confirmation mail
+                        send_activation(request, existing_user, False,
+                                        remember_me)
+                        return render(
+                            request,
+                            'authentication/account_activation_sent.html',
+                            {'is_new': False})
+                # If user doesn't exist, create account and
+                # send confirmation mail
                 if len(existing_users) == 0:
                     user.email = form.cleaned_data['email']
                     user.username = get_random_string(20)
                     user.is_active = False
                     user.save()
                     send_activation(request, user, True, remember_me)
-                    return render(request, 'authentication/account_activation_sent.html', {'is_new': True})
+                    return render(
+                        request, 'authentication/account_activation_sent.html',
+                        {'is_new': True})
         else:
             # # Form is not valid.
             context = {}
@@ -119,6 +135,7 @@ def login(request, base_template=None):
     else:
         form = SignUpForm()
     return render(request, 'authentication/login.html', {'form': form})
+
 
 def send_activation(request, user, is_new, remember_me):
     """Send user an activation/login link.
@@ -129,14 +146,17 @@ def send_activation(request, user, is_new, remember_me):
     """
     current_site = get_current_site(request)
     subject = 'Votre compte à {dom}'.format(dom=current_site.domain)
-    message = render_to_string('authentication/account_activation_email.html', {
-        'user_id': user.pk,
-        'domain': current_site.domain,
-        'token': make_timed_token(user.pk, 20),
-        'is_new': is_new,
-        'remember_me': remember_me,
-    })
-    if hasattr(settings, 'ROLE') and settings.ROLE in ['staging', 'production']:
+    message = render_to_string(
+        'authentication/account_activation_email.html',
+        {
+            'user_id': user.pk,
+            'domain': current_site.domain,
+            'token': make_timed_token(user.pk, 20),
+            'is_new': is_new,
+            'remember_me': remember_me,
+        })
+    if hasattr(settings, 'ROLE') and \
+            settings.ROLE in ['beta', 'production']:
         user.email_user(subject, message)
     elif hasattr(settings, 'ROLE') and settings.ROLE == 'test':
         print("  [Envoi de mél supprimé ici.]")
@@ -144,6 +164,7 @@ def send_activation(request, user, is_new, remember_me):
         # We're in dev.
         print("Mode dev : mél qui aurait été envoyé :")
         print(message)
+
 
 def activate(request, token, remember_me, base_template=None):
     """Process an activation token.
@@ -154,21 +175,25 @@ def activate(request, token, remember_me, base_template=None):
     """
     (user_id, persistent,) = token_valid(token)
     if user_id < 0:
-        return render(request, 'authentication/account_activation_invalid.html')
+        return render(request,
+                      'authentication/account_activation_invalid.html')
     try:
         user = User.objects.get(pk=user_id)
         user.is_active = True
         user.profile.email_confirmed = True
         user.save()
         auth.login(request, user)
-        # Set session cookie expiration to session duration if "False" otherwise for 30 days
+        # Set session cookie expiration to session duration if "False"
+        # otherwise for 30 days
         if remember_me == "False":
             request.session.set_expiry(0)
         else:
             request.session.set_expiry(60*60*24*30)
         return redirect('index')
     except (TypeError, ValueError, OverflowError, User.DoesNotExist):
-        return render(request, 'authentication/account_activation_invalid.html')
+        return render(
+            request, 'authentication/account_activation_invalid.html')
+
 
 class DeauthView(LogoutView):
     """Log out the user.
@@ -181,12 +206,14 @@ class DeauthView(LogoutView):
 def profile(request):
     if request.method == 'POST':
         u_form = UserUpdateForm(request.POST, instance=request.user)
-        p_form = ProfileUpdateForm(request.POST, request.FILES, instance=request.user.profile)
+        p_form = ProfileUpdateForm(request.POST, request.FILES,
+                                   instance=request.user.profile)
 
         if u_form.is_valid() and p_form.is_valid():
             u_form.save()
             p_form.save()
-            # messages.success(request, f'Your Profile has been Updated Successfully')
+            # messages.success(request, f'Your Profile has \
+            # been Updated Successfully')
             return redirect('authentication:mod')
     else:
         u_form = UserUpdateForm(instance=request.user)
