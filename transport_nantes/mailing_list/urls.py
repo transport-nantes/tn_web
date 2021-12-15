af4290f2cf8246881c9b6ae2d624579f0db2d4ef
Benjamin Mourgues <benjamin@mobilitains.fr>
2021-12-15 15:28:22 +0100

    Refactor views_m.py and views.py in Mailing list app
    Merge views_m.py in views.py to reduce confusion in the naming
    Make views.py PEP8 compliant.
    
    Part of #342

--- transport_nantes/mailing_list/urls.py
+++ transport_nantes/mailing_list/urls.py
@@ -1,22 +1,26 @@
 from django.urls import path
 from django.conf import settings
-from .views_m import *
-# from asso_tn.views import AssoView
-from .forms import QuickPetitionSignupForm
+from .views import (MailingListSignup, QuickMailingListSignup,
+                    QuickPetitionSignup, PetitionView, MailingListMerci,
+                    DashboardView)
 
 app_name = 'mailing_list'
 urlpatterns = [
-    path('inscrire', MailingListSignupM.as_view(), name='list_signup'),
-    path('inscrire-captcha', QuickMailingListSignupM.as_view(), name='quick_list_signup'),
+    path('inscrire', MailingListSignup.as_view(),
+         name='list_signup'),
+    path('inscrire-captcha', QuickMailingListSignup.as_view(),
+         name='quick_list_signup'),
 
-    path('petition-captcha', QuickPetitionSignup.as_view(), name='quick_petition_signup'),
+    path('petition-captcha', QuickPetitionSignup.as_view(),
+         name='quick_petition_signup'),
     path('petition/<slug:petition_slug>/', PetitionView.as_view(
         template_name='mailing_list/petition4.html'),
-         name='petition'),
+        name='petition'),
 
     path('dashboard', DashboardView.as_view(), name='dashboard')
 ]
 
 # For debugging the thankyou template:
 if hasattr(settings, 'ROLE') and 'production' != settings.ROLE:
-    urlpatterns.append(path('merci', MailingListMerciM.as_view(), name='list_ok'))
+    urlpatterns.append(path('merci', MailingListMerci.as_view(),
+                            name='list_ok'))
--- transport_nantes/mailing_list/views.py
+++ transport_nantes/mailing_list/views.py
@@ -1,19 +1,21 @@
 from django.contrib.auth.models import User
 from django.core.exceptions import ObjectDoesNotExist
+from django.http import HttpResponseNotFound
 from django.shortcuts import render
+from django.shortcuts import get_object_or_404
+from django.utils.crypto import get_random_string
 from django.views.generic.base import TemplateView
 from django.views.generic.edit import FormView
-# from django.urls import reverse
-# from django.urls import reverse_lazy
-from django.utils.crypto import get_random_string
-from .forms import MailingListSignupForm, QuickMailingListSignupForm
-from .models import MailingList, MailingListEvent
 
-# Create your views here.
+from asso_tn.views import AssoView
+from .forms import MailingListSignupForm, QuickPetitionSignupForm
+from .models import MailingList, MailingListEvent, Petition
+from .events import user_subscribe_count, subscriber_count
+
 
 class MailingListSignup(FormView):
-    template_name = 'mailing_list/signup.html'
-    merci_template = 'mailing_list/merci.html'
+    template_name = 'mailing_list/signup_m.html'
+    merci_template = 'mailing_list/merci_m.html'
     form_class = MailingListSignupForm
     # success_url = reverse_lazy('mailing_list:list_ok')
 
@@ -24,14 +26,16 @@ class MailingListSignup(FormView):
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context['hero'] = True
-        context['hero_image'] = 'asso_tn/images-libres/black-and-white-bridge-children-194009-1000.jpg'
+        context['hero_image'] = ('asso_tn/images-libres/'
+                                 'black-and-white-bridge-children-194009-1000'
+                                 '.jpg')
         context['hero_title'] = 'Newsletter'
         return context
 
     def form_valid(self, form):
         # This method is called when valid form data has been POSTed.
         # It should return an HttpResponse.
-        #form.send_email()
+        # form.send_email()
         subscribe = False
         user = form.save(commit=False)
         try:
@@ -40,7 +44,8 @@ class MailingListSignup(FormView):
             print('ObjectDoesNotExist')
             pass            # I'm not sure this can ever happen.
         if user is None or user.pk is None:
-            user = User.objects.filter(email=form.cleaned_data['email']).first()
+            user = User.objects.filter(
+                email=form.cleaned_data['email']).first()
             if user is None:
                 user = User()   # New user.
                 user.username = get_random_string(20)
@@ -72,15 +77,18 @@ class MailingListSignup(FormView):
                 {
                     'hero': True,
                     'hero_image':
-                    'asso_tn/images-libres/black-and-white-bridge-children-194009-1000.jpg',
+                    ('asso_tn/images-libres/'
+                     'black-and-white-bridge-children-194009-1000'
+                     '.jpg'),
                     'hero_title': 'Newsletter',
                 }
             )
         return super(MailingListSignup, self).form_valid(form)
 
+
 class QuickMailingListSignup(FormView):
-    template_name = 'mailing_list/quick_signup.html'
-    merci_template = 'mailing_list/merci.html'
+    template_name = 'mailing_list/quick_signup_m.html'
+    merci_template = 'mailing_list/merci_m.html'
     form_class = MailingListSignupForm
 
     # We don't currently populate this form with the user's current
@@ -90,7 +98,9 @@ class QuickMailingListSignup(FormView):
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context['hero'] = True
-        context['hero_image'] = 'asso_tn/images-libres/black-and-white-bridge-children-194009-1000.jpg'
+        context['hero_image'] = ('asso_tn/images-libres/'
+                                 'black-and-white-bridge-children-194009-1000'
+                                 '.jpg')
         context['hero_title'] = 'Newsletter'
         return context
 
@@ -122,7 +132,8 @@ class QuickMailingListSignup(FormView):
             print('ObjectDoesNotExist')
             pass            # I'm not sure this can ever happen.
         if user is None or user.pk is None:
-            user = User.objects.filter(email=form.cleaned_data['email']).first()
+            user = User.objects.filter(
+                email=form.cleaned_data['email']).first()
             if user is None:
                 user = User()   # New user.
                 user.username = get_random_string(20)
@@ -154,11 +165,14 @@ class QuickMailingListSignup(FormView):
                 {
                     'hero': True,
                     'hero_image':
-                    'asso_tn/images-libres/black-and-white-bridge-children-194009-1000.jpg',
+                    ('asso_tn/images-libres/'
+                     'black-and-white-bridge-children-194009-1000'
+                     '.jpg'),
                     'hero_title': 'Newsletter',
                 }
             )
         return super(MailingListSignup, self).form_valid(form)
+
 
 class MailingListMerci(TemplateView):
     """View class only used whilst debugging.
@@ -168,13 +182,108 @@ class MailingListMerci(TemplateView):
     the form.
 
     """
-    template_name = 'mailing_list/merci.html'
+    template_name = 'mailing_list/merci_m.html'
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context['hero'] = True
-        context['hero_image'] = 'asso_tn/images-libres/black-and-white-bridge-children-194009-1000.jpg'
+        context['hero_image'] = ('asso_tn/images-libres/'
+                                 'black-and-white-bridge-children-194009-1000'
+                                 '.jpg')
         context['hero_title'] = 'Newsletter'
         context['hero_description'] = '(actuellement debugging)'
         return context
 
+
+class QuickPetitionSignup(FormView):
+    template_name = 'mailing_list/quick_signup_m.html'
+    # This needs to be parameterised by petition.
+    merci_template = 'mailing_list/merci_petition.html'
+    form_class = QuickPetitionSignupForm
+
+    # We don't currently populate this form with the user's current
+    # subscriptions.  If the user is logged in, we should.  This then
+    # becomes the edit form as well.
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['hero'] = True
+        context['hero_image'] = ('asso_tn/images-libres/'
+                                 'black-and-white-bridge-children-194009-1000'
+                                 '.jpg')
+        context['hero_title'] = 'Newsletter'
+        return context
+
+    def form_valid(self, form):
+        """Process a valid QuickPetitionSignup.
+
+        This method is called when valid form data has been POSTed.
+        It returns an HttpResponse.
+        """
+        user = User.objects.filter(email=form.cleaned_data['email']).first()
+        if user is None:
+            user = User()   # New user.
+            user.username = get_random_string(20)
+            user.first_name = form.cleaned_data['first_name'] or 'firstname'
+            user.last_name = form.cleaned_data['last_name'] or 'lastname'
+            user.email = form.cleaned_data['email']
+            user.save()
+        # user.profile.commune = form.cleaned_data['commune']
+        # user.profile.code_postal = form.cleaned_data['code_postal']
+        # user.profile.save()
+
+        petition = MailingList.objects.filter(
+            mailing_list_token=form.cleaned_data['petition_name'])
+        if len(petition) == 0:
+            return HttpResponseNotFound("Pétition inconnu")
+        subscription = MailingListEvent.objects.create(
+            user=user,
+            mailing_list=petition[0],
+            event_type=MailingListEvent.EventType.SUBSCRIBE)
+        subscription.save()
+        return render(
+            self.request, self.merci_template,
+            {
+                'hero': True,
+                'hero_image':
+                ('asso_tn/images-libres/'
+                 'black-and-white-bridge-children-194009-1000'
+                 '.jpg'),
+                'hero_title': 'Newsletter',
+            }
+        )
+
+
+class PetitionView(AssoView):
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        # Need to look up petition here.
+        # Fetch Markdown for entire petition.
+        # Add to context.
+        petition = get_object_or_404(Petition, slug=kwargs['petition_slug'])
+        petition.set_context(context)
+        context['body_text_1'] = petition.petition1_md
+        context['body_text_2'] = petition.petition2_md
+        context['body_text_3'] = petition.petition3_md
+        context['body_text_4'] = petition.petition4_md
+        context['petition'] = petition
+        context['petition_token'] = petition.mailing_list.mailing_list_token
+        # context['hero_image'] = 'asso_tn/traffic-1600.jpg'
+        context['page'] = {'hero_image': 'asso_tn/traffic-1600.jpg',
+                           'hero_title': "C'est l'heure de changer"}
+        return context
+
+
+class DashboardView(TemplateView):
+    template_name = 'mailing_list/dashboard.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        lists = MailingList.objects.all().order_by(
+            'is_petition', 'mailing_list_name')
+        context['mailing_lists'] = [(list, user_subscribe_count(list))
+                                    for list in lists if not list.is_petition]
+        context['petitions'] = [(list, subscriber_count(list))
+                                for list in lists if list.is_petition]
+        return context
