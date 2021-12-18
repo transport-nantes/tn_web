497d5850491de1ed4df2478962b6a774e1cfe139
Jeff Abrahamson <jeff@p27.eu>
2021-12-18 19:27:18 +0100

    Display some basic sessions statistics
    This is still rather approximative, and its goal is to help us get
    familiar with what data is useful to display.  It will also be worth
    doing some further testing on the actual data to convince ourselves
    that we are displaying what we think we are.
    
    Part of #347.

---
+++ transport_nantes/utm/templatetags/utm.py
@@ -0,0 +1,69 @@
+from django import template
+import datetime
+
+from django.db.models import Count, Max
+from utm.models import UTM
+
+register = template.Library()
+
+@register.inclusion_tag('utm/visit_overview.html', takes_context=True)
+def visit_overview(context):
+    """Provide a short summary of page visits.
+    """
+    request = context['request']
+    path = request.path
+    robots = UTM.objects.filter(base_url=path, ua_is_bot=True)
+    humans = UTM.objects.filter(base_url=path, ua_is_bot=False)
+    humans_unique = UTM.objects.filter(base_url=path, ua_is_bot=False) \
+                               .values('session_id').distinct()
+    context['robots_48h'] = robots.filter(
+        timestamp__gt=(datetime.datetime.now() - datetime.timedelta(hours=48))).\
+        count()
+    context['humans_48h'] = humans.filter(
+        timestamp__gt=(datetime.datetime.now() - datetime.timedelta(hours=48))).\
+        count()
+    context['humans_unique_48h'] = humans_unique.filter(
+        timestamp__gt=(datetime.datetime.now() - datetime.timedelta(hours=48))).\
+        count()
+
+    context['robots_7j'] = robots.filter(
+        timestamp__gt=(datetime.datetime.now() - datetime.timedelta(days=7))).\
+        count()
+    context['humans_7j'] = humans.filter(
+        timestamp__gt=(datetime.datetime.now() - datetime.timedelta(days=7))).\
+        count()
+    context['humans_unique_7j'] = humans_unique.filter(
+        timestamp__gt=(datetime.datetime.now() - datetime.timedelta(days=7))).\
+        count()
+
+    context['robots'] = robots.count()
+    context['humans'] = humans.count()
+    context['humans_unique'] = humans_unique.count()
+
+    sources = UTM.objects.filter(base_url=path, ua_is_bot=False) \
+                         .values('source').distinct()
+    context['sources'] = {}
+    for source in sources:
+        this_source = source['source']
+        the_count = UTM.objects.filter(
+            base_url=path, source=this_source, ua_is_bot=False) \
+                               .values('session_id').distinct().count()
+        if "" == this_source:
+            # This should be doable with the |default filter in the template
+            # but that's not working for me.
+            this_source = "<aucun>"
+        context['sources'][this_source] = the_count
+    mediums = UTM.objects.filter(base_url=path, ua_is_bot=False) \
+                         .values('medium').distinct()
+    context['mediums'] = {}
+    for medium in mediums:
+        this_medium = medium['medium']
+        the_count = UTM.objects.filter(
+            base_url=path, medium=this_medium, ua_is_bot=False) \
+                               .values('session_id').distinct().count()
+        if "" == this_medium:
+            # This should be doable with the |default filter in the template
+            # but that's not working for me.
+            this_medium = "<aucun>"
+        context['mediums'][this_medium] = the_count
+    return context
