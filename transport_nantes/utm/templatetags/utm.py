d132f2774c2224b0d2a0433860a97b1c2dc5b840
Jeff Abrahamson <jeff@p27.eu>
2022-01-03 15:46:01 +0100

    Make timestamps TZ-aware in queries
    Part of #347.

--- transport_nantes/utm/templatetags/utm.py
+++ transport_nantes/utm/templatetags/utm.py
@@ -1,6 +1,6 @@
 from django import template
 import datetime
-
+from django.utils.timezone import make_aware
 from django.db.models import Count, Max
 from utm.models import UTM
 
@@ -10,6 +10,7 @@ register = template.Library()
 def visit_overview(context):
     """Provide a short summary of page visits.
     """
+    time_now = make_aware(datetime.datetime.now())
     request = context['request']
     path = request.path
     robots = UTM.objects.filter(base_url=path, ua_is_bot=True)
@@ -17,23 +18,23 @@ def visit_overview(context):
     humans_unique = UTM.objects.filter(base_url=path, ua_is_bot=False) \
                                .values('session_id').distinct()
     context['robots_48h'] = robots.filter(
-        timestamp__gt=(datetime.datetime.now() - datetime.timedelta(hours=48))).\
+        timestamp__gt=(time_now - datetime.timedelta(hours=48))).\
         count()
     context['humans_48h'] = humans.filter(
-        timestamp__gt=(datetime.datetime.now() - datetime.timedelta(hours=48))).\
+        timestamp__gt=(time_now - datetime.timedelta(hours=48))).\
         count()
     context['humans_unique_48h'] = humans_unique.filter(
-        timestamp__gt=(datetime.datetime.now() - datetime.timedelta(hours=48))).\
+        timestamp__gt=(time_now - datetime.timedelta(hours=48))).\
         count()
 
     context['robots_7j'] = robots.filter(
-        timestamp__gt=(datetime.datetime.now() - datetime.timedelta(days=7))).\
+        timestamp__gt=(time_now - datetime.timedelta(days=7))).\
         count()
     context['humans_7j'] = humans.filter(
-        timestamp__gt=(datetime.datetime.now() - datetime.timedelta(days=7))).\
+        timestamp__gt=(time_now - datetime.timedelta(days=7))).\
         count()
     context['humans_unique_7j'] = humans_unique.filter(
-        timestamp__gt=(datetime.datetime.now() - datetime.timedelta(days=7))).\
+        timestamp__gt=(time_now - datetime.timedelta(days=7))).\
         count()
 
     context['robots'] = robots.count()
