d2ef39ee74d7dbfcfc9d30faf055d7e792e787bc
mickael anicette <mickaelanicette@gmail.com>
2022-02-19 19:59:02 -0300

    Add update delete view for press
    Part #474

--- transport_nantes/press/form.py
+++ transport_nantes/press/form.py
@@ -1,7 +1,10 @@
 from django import forms
+from .models import PressMention
 
-
-class PressMentionForm(forms.Form):
+class PressMentionForm(forms.ModelForm):
+    class Meta:
+        model = PressMention
+        fields = "__all__"
     newspaper_name = forms.CharField(max_length=200, label="Nom du journal")
     article_link = forms.URLField(max_length=255, label="Lien de l'article")
     article_title = forms.CharField(max_length=200, label="Titre de l'article")
