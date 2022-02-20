ae9e6f370779cf2d1c45272791961b9866b34413
mickael anicette <mickaelanicette@gmail.com>
2022-02-20 11:01:27 -0300

    Add datalist for the newspaper name field
    Part #474

--- transport_nantes/press/form.py
+++ transport_nantes/press/form.py
@@ -1,5 +1,8 @@
 from django import forms
 from .models import PressMention
+from crispy_forms.helper import FormHelper
+from crispy_forms.layout import Layout, Field, ButtonHolder, Submit, HTML
+
 
 class PressMentionForm(forms.ModelForm):
     class Meta:
@@ -14,3 +17,25 @@ class PressMentionForm(forms.ModelForm):
     article_publication_date = forms.DateField(
         label="Date de pubication de l'article",
         widget=forms.TextInput(attrs={'type': 'date'}))
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.helper = FormHelper()
+        self.helper.form_method = "post"
+        self.helper.layout = Layout(
+            Field("newspaper_name", list="newspaper-names"),
+            HTML("<datalist id='newspaper-names'>"
+                 "{%for newspaper_name in newspaper_name_list %}"
+                 "<option value='{{newspaper_name}}'>"
+                 "{% endfor %}"
+                 "</datalist>"
+                 ),
+            Field("article_link"),
+            Field("article_title"),
+            Field("article_summary"),
+            Field("article_publication_date"),
+            ButtonHolder(
+                Submit('submit', 'Sauvegarder',
+                       css_class='btn navigation-button btn-lg btn-block')
+            )
+        )
