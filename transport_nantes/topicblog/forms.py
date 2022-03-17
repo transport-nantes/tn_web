cf4af41cbb098dfeee0268b0c41b114e45f21421
mickael anicette <mickaelanicette@gmail.com>
2022-03-17 12:29:48 -0300

    Add TopicBlogEmail add/edit page
    Add template_config in TopicBlogEmail to control fields constraints
    for publication.
    
    Modify view to preserve images on edit.
    
    Part #531

--- transport_nantes/topicblog/forms.py
+++ transport_nantes/topicblog/forms.py
@@ -1,6 +1,6 @@
 from django import forms
 from django.forms import ModelForm
-from .models import TopicBlogItem, TopicBlogLauncher
+from .models import TopicBlogItem, TopicBlogLauncher, TopicBlogEmail
 from mailing_list.models import MailingList
 
 
@@ -81,6 +81,44 @@ class TopicBlogLauncherForm(ModelForm):
         # When an item is published, its slug becomes frozen (unmodifiable).
         if self.instance.publication_date:
             self.fields['slug'].widget.attrs['readonly'] = True
+
+        def get_template_list(self) -> list:
+
+            templates = self.instance.template_config
+            template_list = \
+                [(None, "Selectionnez un template ...")]
+            for template, value in templates.items():
+                template_list.append((template, value["user_template_name"]))
+
+            return template_list
+
+        template_list = get_template_list(self)
+        self.fields['template_name'] = forms.ChoiceField(
+            choices=template_list,
+            initial=self.instance.template_name)
+
+
+class TopicBlogEmailForm(ModelForm):
+    """
+    Generates a form to create and edit TopicsBlog Email objects.
+    """
+
+    class Meta:
+        model = TopicBlogEmail
+        # Admins can still edit those values
+        exclude = ('first_publication_date', 'publisher', 'user',
+                   'publication_date')
+    field_order = ["slug", "subject", "title", "header_title",
+                   "header_description", "header_image", "template_name",
+                   "body_text_1_md", "cta_1_slug", "cta_1_label",
+                   "body_image_1", "body_image_1_alt_text",
+                   "social_description", "twitter_title",
+                   "twitter_description", "twitter_image", "og_title",
+                   "og_description", "og_image", "author_notes"]
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.instance: TopicBlogLauncher
 
         def get_template_list(self) -> list:
 
--- transport_nantes/topicblog/models.py
+++ transport_nantes/topicblog/models.py
@@ -598,6 +598,51 @@ class TopicBlogEmail(TopicBlogObjectSocialBase):
             ("tbe.may_send", "May send TopicBlogEmails"),
             ("tbe.may_send_self", "May send own TopicBlogEmails"),
         )
+    # Plus slug, template, title, comment, and social media fields,
+    # provided through abstract base class.
+
+    template_config_default = {
+        "optional_fields_for_publication": (
+            'header_image', 'header_description',
+            'cta_1_slug', 'cta_1_label',
+            'cta_2_slug', 'cta_2_label',
+            'body_image_1', 'body_image_1_alt_text',
+            'body_image_2', 'body_image_2_alt_text',
+            'twitter_title', 'twitter_description',
+            'twitter_image', 'og_title',
+            'og_description', 'og_image'
+        ),
+        "one_of_fields_for_publication": [
+            ['header_title', 'header_description'],
+            ['body_text_1_md', 'body_text_2_md'],
+        ],
+        # Dependent fields: if one in a group is provided, the others must
+        # be as well before we can publish.
+        "dependent_field_names": [
+            ['body_image_1', 'body_image_1_alt_text'],
+            ['body_image_2', 'body_image_2_alt_text'],
+            ['cta_1_slug', 'cta_1_label'],
+            ['cta_2_slug', 'cta_2_label'],
+        ],
+    }
+    template_config = {
+        'topicblog/content_email.html': {
+            'user_template_name': 'Classic',
+            'active': True,
+            "fields": {
+                'slug': True,
+                'title': True,
+                'subject': True,
+                'body_text_1_md': True,
+            },
+            "optional_fields_for_publication":
+                template_config_default['optional_fields_for_publication'],
+            "one_of_fields_for_publication":
+                template_config_default['one_of_fields_for_publication'],
+            "dependent_field_names":
+                template_config_default['dependent_field_names'],
+        },
+    }
 
     subject = models.CharField(max_length=80, blank=True)
     header_image = models.ImageField(
@@ -867,8 +912,8 @@ class TopicBlogLauncher(TopicBlogObjectBase):
                 'article_slug:': True,
                 'campaign_name': True,
                 'launcher_text_md:': True,
-                'launcher_image':True,
-                'launcher_image_alt_text':True,
+                'launcher_image': True,
+                'launcher_image_alt_text': True,
             },
             "optional_fields_for_publication":
                 template_config_default['optional_fields_for_publication'],
