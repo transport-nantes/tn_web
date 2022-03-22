790db6711df38787821fbe9e3d09b95ff52069df
Jeff Abrahamson <jeff@p27.eu>
2022-03-22 07:52:27 +0100

    Alphabetic substitution and comments
    Two junior engineers have wasted significant time interacting with the
    timed token functions.  Either the comments, including slightly
    misleading self-documenting variables names, were too misleading or
    else the use of mild cryptography, taken for granted by those with a
    theoretical computer science background, presented too great a mental
    obstacle.  Or something else.
    
    This commit does not modify behaviour: it's goal is only to render the
    function pair (make and validate) clearer to new junior devs.

--- transport_nantes/asso_tn/utils.py
+++ transport_nantes/asso_tn/utils.py
@@ -14,41 +14,53 @@ from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 logger = logging.getLogger("django")
 
 
-def make_timed_token(email, minutes, persistent=0, test_value_now=None):
+def make_timed_token(string_key, minutes, int_key=0, test_value_now=None):
     """Make a URL-safe timed token that's valid for minutes minutes.
 
-    This is meant for use with confirmation mails to avoid having to
-    hold state on the server.
+    This was originally meant for use with confirmation mails to avoid
+    having to hold state on the server: the string_key is the user's
+    email address, the int_key is 0 or 1, according to whether the
+    user checked "remember me".
 
-    The token is tied to a specific user (identified by email address)
-    to avoid frauds where a token is used to validate a different user
-    than the one intended (i.e., to validate a user whose email the
-    person doesn't control or by simple fishing for email).  We insert
-    a random number (and our own secret key) to avoid replay attacks.
+    In this case (auth), the token is tied to a specific user
+    (identified by email address, the string_key) to avoid frauds
+    where a token is used to validate a different user than the one
+    intended (i.e., to validate a user whose email the person doesn't
+    control or by simple fishing for email).  We insert a random
+    number (and our own secret key) to avoid replay attacks.
 
-    The integer persistent is meant for setting session cookies, but
-    these functions make no assumption other than that it is an
-    integer.  The auth functions should assume that 0 means unchecked
-    "remember me" and 1 means "remember me" and anything else is
-    invalid.
+    The integer int_key was originally conceived for setting session
+    cookies, but these functions make no assumption other than that it
+    is an integer.  The auth functions should assume that 0 means
+    unchecked "remember me" and 1 means "remember me" and anything
+    else is invalid.
+
+    Note that we use this function and its paired validate function
+    for other tasks as well, notably for mailing list unsubscribe
+    links.
 
     The test_value_now is for testing and should not normally be set.
 
+    Note that we, and therefore anyone, can reverse the token.  The
+    data inserted is not secure or hidden.  We can't even guarantee
+    that we created the token unless there exists a private
+    relationship between string_key and int_key.
+
     """
-    secret = settings.SECRET_KEY
     rand_value = secrets.token_urlsafe(20)
     if test_value_now is not None:
         now = test_value_now
     else:
         now = datetime.datetime.now().timestamp()
     soon_seconds = int_to_base36(int(now + 60 * minutes))
-    persistent_str = int_to_base36(int(persistent))
-    hmac = salted_hmac(soon_seconds + persistent_str, rand_value + str(email)).hexdigest()[:20]
+    int_key_str = int_to_base36(int(int_key))
+    hmac = salted_hmac(soon_seconds + int_key_str, rand_value + str(string_key)
+                       ).hexdigest()[:20]
     token = '{rnd}|{e}|{t}|{p}|{h}'.format(
         rnd=rand_value,
-        e=email,
+        e=string_key,
         t=soon_seconds,
-        p=persistent_str,
+        p=int_key_str,
         h=hmac)
     encoded_token = urlsafe_base64_encode(token.encode())
     return encoded_token
@@ -57,12 +69,12 @@ def make_timed_token(email, minutes, persistent=0, test_value_now=None):
 def token_valid(encoded_timed_token, test_value_now=None):
     """Validate a timed token.
 
-    Return the email of the user for which the token is valid, if the
-    token is valid.  Return None if the token is not (either because
-    it is malformed or because it has expired).
+    Return the string_key for which the token is valid, if the token
+    is valid.  Return None if the token is not (either because it is
+    malformed or because it has expired).
 
-    If the token is valid, also return the persistent integer.  If the
-    token is not valid, persistent will be zero.
+    If the token is valid, also return the int_key integer.  If the
+    token is not valid, int_key will be zero.
 
     The test_value_now is for testing and should not normally be set.
 
@@ -72,10 +84,10 @@ def token_valid(encoded_timed_token, test_value_now=None):
     except (TypeError, ValueError):
         logger.info(f"token_valid: invalid token ({encoded_timed_token})")
         return (None, 0)
-    the_rand_value, the_email, the_soon, the_persistent, the_hmac = \
+    the_rand_value, the_string_key, the_soon, the_int_key, the_hmac = \
         timed_token.split('|')
-    computed_hmac = salted_hmac(the_soon + the_persistent,
-                                the_rand_value + str(the_email)).hexdigest()[:20]
+    computed_hmac = salted_hmac(the_soon + the_int_key,
+                                the_rand_value + str(the_string_key)).hexdigest()[:20]
     if computed_hmac != the_hmac:
         return (None, 0,)
     if test_value_now is not None:
@@ -84,7 +96,7 @@ def token_valid(encoded_timed_token, test_value_now=None):
         now = datetime.datetime.now().timestamp()
     if now > base36_to_int(the_soon):
         return (None, 0)
-    return (the_email, base36_to_int(the_persistent),)
+    return (the_string_key, base36_to_int(the_int_key),)
 
 
 class StaffRequiredMixin(LoginRequiredMixin, UserPassesTestMixin):
