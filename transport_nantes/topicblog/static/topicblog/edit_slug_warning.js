feffa1ff35d153be2062422dd85e99679e14ebcc
mickael anicette <mickaelanicette@gmail.com>
2022-03-25 09:57:04 -0300

    Fix the typo

--- transport_nantes/topicblog/static/topicblog/edit_slug_warning.js
+++ transport_nantes/topicblog/static/topicblog/edit_slug_warning.js
@@ -2,10 +2,10 @@ $(document).ready(function () {
     const DEFAULT_SLUG = $('#id_slug').val();
     console.log(DEFAULT_SLUG)
     $('#id_slug').on('input', function () {
-        if (DEFAULT_SLUG != $(this).val() && !$('#slug_change_warnning').length) {
-            $('#id_slug').after("<span class='small text-danger' id='slug_change_warnning'><i class='fas fa-exclamation-triangle'></i> vous êtes sur le point de modifier le slug</span>");
-        }else if (DEFAULT_SLUG == $(this).val() && $('#slug_change_warnning').length){
-            $("#slug_change_warnning").remove();
+        if (DEFAULT_SLUG != $(this).val() && !$('#slug_change_warning').length) {
+            $('#id_slug').after("<span class='small text-danger' id='slug_change_warning'><i class='fas fa-exclamation-triangle'></i> vous êtes sur le point de modifier le slug</span>");
+        }else if (DEFAULT_SLUG == $(this).val() && $('#slug_change_warning').length){
+            $("#slug_change_warning").remove();
         }
     });
 });
