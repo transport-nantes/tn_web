2583d5f7ee773f13a36fbeb774589ae830d20a35
mickael anicette <mickaelanicette@gmail.com>
2022-03-25 10:23:22 -0300

    Remove immutable status of slug field in TopicBlog Press
    Add a warning if the slug field  change
    
    Close #552

--- transport_nantes/topicblog/forms.py
+++ transport_nantes/topicblog/forms.py
@@ -148,10 +148,6 @@ class TopicBlogPressForm(ModelForm):
         super().__init__(*args, **kwargs)
         self.instance: TopicBlogLauncher
 
-        # When an item is published, its slug becomes frozen (unmodifiable).
-        if self.instance.publication_date:
-            self.fields['slug'].widget.attrs['readonly'] = True
-
         def get_template_list(self) -> list:
 
             templates = self.instance.template_config
