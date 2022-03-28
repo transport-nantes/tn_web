9bdeb56ad4a0630cf4e2412ae57075fdbc551108
mickael anicette <mickaelanicette@gmail.com>
2022-03-28 13:19:06 -0300

    Avoid url reverse lookup error for missing slugs
    The launcher and item_teaser templatetags would pass None to their
    templates instead of a valid object if the slug wasn't found.  That
    led to a 500 on url reverse lookup in their templates.
    
    If the passed object is None, then don't attempt to reverse the url.
    
    Part of #581.

--- transport_nantes/topicblog/templatetags/item_teaser.py
+++ transport_nantes/topicblog/templatetags/item_teaser.py
@@ -1,10 +1,15 @@
 from django import template
 from topicblog.models import TopicBlogItem
+import logging
+
 register = template.Library()
 
+logger = logging.getLogger("django")
 
 @register.inclusion_tag('topicblog/template_tags/item_teaser.html')
 def item_teaser(slug):
     item = TopicBlogItem.objects.filter(
         slug__iexact=slug, publication_date__isnull=False).first()
+    if item is None:
+        logger.error(f"item_teaser failed to find slug: \"{slug}\".")
     return {'item': item}
--- transport_nantes/topicblog/templatetags/launcher.py
+++ transport_nantes/topicblog/templatetags/launcher.py
@@ -1,10 +1,15 @@
 from django import template
 from topicblog.models import TopicBlogLauncher
+import logging
+
 register = template.Library()
 
+logger = logging.getLogger("django")
 
 @register.inclusion_tag('topicblog/template_tags/launcher.html')
 def launcher(slug):
     launcher = TopicBlogLauncher.objects.filter(
         slug__iexact=slug, publication_date__isnull=False).first()
+    if launcher is None:
+        logger.error(f"item_teaser failed to find slug: \"{slug}\".")
     return {'launcher': launcher}
