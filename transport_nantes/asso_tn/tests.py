82d52f1d0c8ffda702558fd0e0679d1db01c11d7
mickael anicette <mickaelanicette@gmail.com>
2022-03-28 13:25:16 -0300

    Add test for the new index page
    This test merely confirms that something loads successfully at '/'.
    It's not what we'll eventually need, but it will remind us to do
    better.
    
    We are staging the new index page at /index2 until we're happy with
    it.  Note that we'll have to update tests again when we make that
    change.
    
    Part of #581.

--- transport_nantes/asso_tn/tests.py
+++ transport_nantes/asso_tn/tests.py
@@ -29,3 +29,15 @@ class TimedTokenTest(TestCase):
             after_response = token_valid(token, k_now + k_expiry_seconds + 1)
             self.assertEqual(after_response[0], None)
             self.assertEqual(after_response[1], 0)
+
+class TestIndex(TestCase):
+
+    def test_index_page(self):
+        """Test the new index page.
+
+        This is far too rudimentary.  We should improve it some day,
+        such as before we make it the default index page.
+
+        """
+        response = self.client.get("/index2")
+        self.assertEqual(response.status_code, 200)
