af20935a30a64ee5774b6439a5a1f3cd24f429b8
Benjamin Mourgues <benjamin@mobilitains.fr>
2022-03-29 14:27:01 +0200

    Add the number of recipients under the dropdown selection

--- transport_nantes/topicblog/static/topicblog/email-sending-confirm.js
+++ transport_nantes/topicblog/static/topicblog/email-sending-confirm.js
@@ -3,6 +3,9 @@ This script is used to display the number of recipients before
 sending a TBEmail object to a mailing list, and ask for confirmation
 */
 
+// Initial state
+document.getElementById('nb-recipients').innerHTML = "0";
+document.getElementById("send-email-btn").disabled = true;
 
 async function get_number_of_recipients(mailing_list_token) {
     let response = await $.ajax({
@@ -12,15 +15,40 @@ async function get_number_of_recipients(mailing_list_token) {
     return response;
 }
 
+//// This Event listener updates the number of recipients message upon selection
+//// of a mailing list
+// This fires when we select a mailing list in the dropdown menu
+document.getElementById('id_mailing_list').addEventListener('change', function() {
+    let mailing_list_token = this.value;
+    console.log(mailing_list_token);
+    if (!mailing_list_token) {
+        document.getElementById('nb-recipients').innerHTML = "0";
+        document.getElementById("send-email-btn").disabled = true;
+    }
+    // Sends the mailing_list_token to the server to get the number of recipients
+    get_number_of_recipients(mailing_list_token).then(function(response) {
+        // The response is a dict with a key "count" and a value of the number of recipients
+        let number_of_recipients = response.count;
+        // The number_of_recipients is identified and isolated in the id "nb-recipients"
+        // We replace its value with the one received from the server.
+        document.getElementById("nb-recipients").innerHTML = number_of_recipients;
+        number_of_recipients = parseInt(number_of_recipients);
+        // If the number of recipients is 0, we disable the send-email-btn
+        if (number_of_recipients == 0) {
+            document.getElementById("send-email-btn").disabled = true;
+        }
+        else {
+            document.getElementById("send-email-btn").disabled = false;
+        }
+    });
+});
+
 // This fires only when the "Envoyer" button is clicked
 document.getElementById('send-email-btn').addEventListener('click', async function (event) {
     // Prevent the button to POST
     event.preventDefault();
-    // Get the selected value in the dropdown
-    let mailing_list_token = document.getElementById('id_mailing_list').value;
-    // Result of the Ajax request is a dict {"count" : <int:number_of_recipients>}
-    let number_of_recipients = await get_number_of_recipients(mailing_list_token);
-    number_of_recipients = number_of_recipients["count"];
+
+    number_of_recipients = parseInt(document.getElementById("nb-recipients").innerHTML);
     if (number_of_recipients > 0) {
         // Display a confirmation alert with OK and cancel,if OK is clicked, it evaluates to True.
         if (confirm("Vous êtes sur le point d'envoyer un email à " + number_of_recipients + " destinataires.\n\n" +
@@ -30,7 +58,5 @@ document.getElementById('send-email-btn').addEventListener('click', async functi
             document.getElementById('send-email-btn').innerHTML = 'Envoi en cours...';
             document.getElementById('send-email-form').submit();
         };
-    } else {
-        alert("Aucun destinataire n'a été trouvé.\n\n");
     }
 })
