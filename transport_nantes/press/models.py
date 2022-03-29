6d92d5de692d70749ca9b7cf1c47179e00a92643
mickael anicette <mickaelanicette@gmail.com>
2022-03-29 18:22:06 -0300

    Add open graph on PressMention Model
    When user add a new press_mention if the form is valid
    we will catch the open graph data and store the values
    on the record.

--- transport_nantes/press/models.py
+++ transport_nantes/press/models.py
@@ -1,6 +1,5 @@
 from django.db import models
 
-
 class PressMention(models.Model):
     class Meta:
         permissions = (
@@ -14,6 +13,12 @@ class PressMention(models.Model):
     article_title = models.CharField(max_length=200)
     article_summary = models.TextField()
     article_publication_date = models.DateField()
+    # OG data SHOULD NOT be blank, but if the site 
+    # can't provide open graph or the user gives the wrong
+    # article link we let the possibility of the data to be blank.
+    og_title = models.CharField(max_length=255, blank=True)
+    og_description = models.TextField(blank=True)
+    og_image = models.ImageField(upload_to="press_mention/open_graph/",blank=True)
 
     def __str__(self):
         return f"{self.newspaper_name} {self.article_title}"
