1fc401d6c609c9e1169919c8b6df6ab456a6ed98
Benjamin Mourgues <benjamin@mobilitains.fr>
2022-03-29 09:58:54 +0200

    Make the number of displayed words adjustable in item-teaser
    It's a proposition to make the number of words displayed adjustable
    depending on what we want to display.
    I think it could be used to show just enough to tease curisity from the
    user, but that line depends on the item we apply the teasing to.
    
    For now it's hardcoded, but so is the item so it's still a git action to
    edit this value.
    However, I think we could add an optional field "teasing_length" to the
    TBItem model to make it configurable in the future.

--- transport_nantes/topicblog/templatetags/item_teaser.py
+++ transport_nantes/topicblog/templatetags/item_teaser.py
@@ -6,10 +6,11 @@ register = template.Library()
 
 logger = logging.getLogger("django")
 
+
 @register.inclusion_tag('topicblog/template_tags/item_teaser.html')
-def item_teaser(slug):
+def item_teaser(slug, truncate_after=50):
     item = TopicBlogItem.objects.filter(
         slug__iexact=slug, publication_date__isnull=False).first()
     if item is None:
         logger.error(f"item_teaser failed to find slug: \"{slug}\".")
-    return {'item': item}
+    return {'item': item, 'truncate_after': truncate_after}
