1a47d1d82e8f1b1bb8e14c6a17d425141b25d978
Jeff Abrahamson <jeff@p27.eu>
2022-04-25 14:05:20 +0200

    Create Photo app and PhotoEntry model
    This model represents a single photo entry for the quartier des
    Hauts-Pavés Saint-Félix's photo contest, aka Opération Piétion.
    
    Part of #696

---
+++ transport_nantes/photo/models.py
@@ -0,0 +1,50 @@
+"""Models for photo competition."""
+
+from django.contrib.auth.models import User
+from django.db import models
+
+
+class PhotoEntry(models.Model):
+    """
+    Represent an entry in a photo competition.
+
+    """
+    class Meta:
+        verbose_name = "Photo Entry"
+        verbose_name_plural = "Photo Entries"
+
+    class CategoryType(models.TextChoices):
+        PIETON_URBAIN = 'PIETON_URBAIN', 'Piéton urbain'
+        LA_VIE_EN_FAMILLE = 'LA_VIE_EN_FAMILLE', 'La vie en famille'
+        L_AMOUR = 'L_AMOUR', "L'amour"
+        LE_TRAVAIL = 'LE_TRAVAIL', 'Le travail'
+        LE_PHOTOJOURNALISME = 'LE_PHOTOJOURNALISME', 'Le photojournalisme'
+        PHOTOGRAPHE_SENIOR = 'PHOTOGRAPHE_SENIOR', 'Photographe senior'
+        JEUNE_PHOTOGRAPHE = 'JEUNE_PHOTOGRAPHE', 'Jeune photographe'
+
+    user = models.ForeignKey(User, on_delete=models.PROTECT)
+    category = models.CharField(
+        max_length=80,
+        choices=CategoryType.choices,
+        verbose_name="Catégorie")
+    relationship_to_competition = models.TextField(
+        blank=True,
+        verbose_name=("Comment cette photo est-elle en relation avec le "
+                      "quartier des Hauts-Pavés ?"))
+    photo_location = models.CharField(
+        max_length=80, blank=True,
+        verbose_name="Lieu de la photo")
+    photo_kit = models.TextField(blank=True, verbose_name="Appareil photo")
+    technical_notes = models.TextField(
+        blank=True,
+        verbose_name="Notes techniques")
+    photographer_comments = models.TextField(
+        blank=True,
+        verbose_name="Commentaires du photographe")
+    submitted_photo = models.ImageField(
+        upload_to='photo/', blank=False,
+        help_text='résolution minimale recommandée : 1800 x 1800',
+        verbose_name="Photo")
+
+    def __str__(self):
+        return f"{self.user.username} - {self.category}"
---
+++ transport_nantes/photo/urls.py
@@ -0,0 +1,19 @@
+from django.urls import path
+from . import views
+
+"""
+Application to manage a photo competition.
+
+In the first iteration of the application, we assume a single photo
+competition in all of history.  If we repeat, we'll add
+database-driven concepts of which competition and so forth.
+
+"""
+
+app_name = 'photo'
+urlpatterns = [
+    path('upload/<str:categories>/', views.UploadEntry.as_view(),
+         name='upload'),
+    path('confirmation/', views.Confirmation.as_view(),
+         name='confirmation'),
+]
