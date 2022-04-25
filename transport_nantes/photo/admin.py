68f9fecaefca3ad51dafb83eb7020df12f13da51
Benjamin Mourgues <benjamin@mobilitains.fr>
2022-04-25 15:56:00 +0200

    Add PhotoEntry sorting and filter in admin panel
    Part of #696

--- transport_nantes/photo/admin.py
+++ transport_nantes/photo/admin.py
@@ -1,8 +1,13 @@
 from django.contrib import admin
 from .models import PhotoEntry
 
+
 class PhotoEntryAdmin(admin.ModelAdmin):
-    pass
+    readonly_fields = ('pk',)
+
+    list_display = ("category", "user")
+    list_filter = ("category", "user")
+    search_fields = ("category", "user")
 
 
 admin.site.register(PhotoEntry, PhotoEntryAdmin)
