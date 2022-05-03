53e50404034083b00f7e0a3bdbd829a0de9e2123
Jeff Abrahamson <jeff@p27.eu>
2022-05-03 18:46:57 +0200

    Use console email backend in dev

--- transport_nantes/transport_nantes/settings.py
+++ transport_nantes/transport_nantes/settings.py
@@ -120,7 +120,10 @@ AUTH_PASSWORD_VALIDATORS = [
 ]
 PASSWORD_RESET_TIMEOUT_DAYS = 1
 
-EMAIL_BACKEND = 'django_amazon_ses.EmailBackend'
+if ROLE == 'dev':
+    EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+else:
+    EMAIL_BACKEND = 'django_amazon_ses.EmailBackend'
 AWS_ACCESS_KEY_ID = settings_local.AWS_ACCESS_KEY_ID
 AWS_SECRET_ACCESS_KEY = settings_local.AWS_SECRET_ACCESS_KEY
 AWS_DEFAULT_REGION = settings_local.AWS_DEFAULT_REGION
