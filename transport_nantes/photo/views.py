01c5e4a3f77583e444ef0c9a81d0dca1d54730ba
Benjamin Mourgues <benjamin@mobilitains.fr>
2022-05-04 15:02:13 +0200

    Subscribe users to mailing list on photo submission

--- transport_nantes/photo/views.py
+++ transport_nantes/photo/views.py
@@ -1,10 +1,15 @@
 """Application to manage a photo competition."""
-
+import logging
 from django.urls import reverse_lazy
 from django.views.generic import (TemplateView, CreateView)
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django.core.exceptions import ObjectDoesNotExist
 from .forms import PhotoEntryForm
 from .models import PhotoEntry
+from mailing_list.models import MailingList
+from mailing_list.events import subscribe_user_to_list
+
+logger = logging.getLogger("django")
 
 
 class UploadEntry(LoginRequiredMixin, CreateView):
@@ -24,6 +29,14 @@ class UploadEntry(LoginRequiredMixin, CreateView):
         self.object = None
         form.instance.user = request.user
         if form.is_valid():
+            try:
+                mailing_list = MailingList.objects.get(
+                    mailing_list_token="operation-pieton")
+                user = request.user
+                subscribe_user_to_list(user, mailing_list)
+            except ObjectDoesNotExist:
+                logger.info("Mailing list operation-pieton does not exist")
+
             return self.form_valid(form)
         else:
             return self.form_invalid(form)
