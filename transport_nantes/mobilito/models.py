3c12888a4ebd98aac1dc27f041649a703ee86ad0
Benjamin Mourgues <benjamin@mobilitains.fr>
2022-06-23 16:50:07 +0200

    Add a landing page and a tutorial page for Mobilito
    Also add a MobilitoUser model so we can track when users have read the
    tutorial, though that's not used or implemented yet.
    
    Part of #40

--- transport_nantes/mobilito/models.py
+++ transport_nantes/mobilito/models.py
@@ -1,3 +1,10 @@
+from django.contrib.auth.models import User
 from django.db import models
 
-# Create your models here.
+
+class MobilitoUser(models.Model):
+    """Maintain information about Mobilito users."""
+
+    user = models.ForeignKey(User, on_delete=models.PROTECT, related_name='+')
+    # Set first_time to False once the user has read the tutorial.
+    first_time = models.BooleanField(default=True)
--- transport_nantes/mobilito/views.py
+++ transport_nantes/mobilito/views.py
@@ -1,5 +1,21 @@
+from django.http import HttpResponseRedirect
+from django.urls import reverse
 from django.views.generic import TemplateView
 
 
 class MobilitoView(TemplateView):
+    """Present Mobilito landing page.
+
+    This is far too simple, but it will stand in for the moment.
+    """
+
     template_name = 'mobilito/index.html'
+
+
+class TutorialView(TemplateView):
+    """Present the tutorial.
+
+    This is far too simple, but it will stand in for the moment.
+    """
+
+    template_name = 'mobilito/tutorial.html'
