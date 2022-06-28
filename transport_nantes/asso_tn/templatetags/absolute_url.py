59d5420ccbde6aea9315d85d5b9760b249516e75
Benjamin Mourgues <benjamin@mobilitains.fr>
2022-06-28 10:35:10 +0200

    Add Hero image on top of emails and absolute_url_image template tag
    The template tag allows an easier testing in dev mode by displaying
    images in test emails (default url wouldn't include the host)
    
    Part of #829

--- transport_nantes/asso_tn/templatetags/absolute_url.py
+++ transport_nantes/asso_tn/templatetags/absolute_url.py
@@ -9,3 +9,10 @@ def absolute_url(context, view_name, *args, **kwargs):
     request = context['request']
     return request.build_absolute_uri(
         reverse(view_name, args=args, kwargs=kwargs))
+
+
+@register.simple_tag(takes_context=True)
+def absolute_url_image(context, image_url):
+    request = context['request']
+    return request.build_absolute_uri(
+        image_url)
