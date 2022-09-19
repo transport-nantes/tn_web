707f48a9fb96dded8577d027ae109c08227f0d4f
Jeff Abrahamson <jeff@p27.eu>
2022-09-19 08:35:27 +0200

    Remove old utm visit data
    It has been superceded by gom jabbar.
    
    Part of #926.

--- transport_nantes/utm/admin.py
+++ transport_nantes/utm/admin.py
@@ -1,12 +1,2 @@
 from django.contrib import admin
 
-from .models import Visit
-
-
-class UtmAdmin(admin.ModelAdmin):
-    list_display = ('session_id', 'timestamp', 'ua_is_bot',
-                    'source', 'campaign', 'content', 'base_url')
-    search_fields = ('source', 'campaign', 'content', 'base_url', 'session_id')
-
-
-admin.site.register(Visit, UtmAdmin)
