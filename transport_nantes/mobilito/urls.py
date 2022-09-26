e79dc5e0bdb4658326badaf70d474a3042b29f1e
Benjamin Mourgues <benjamin@mobilitains.fr>
2022-09-26 17:01:52 +0200

    Add a view to see Sessions summary with UUID
    This view allows the user to see a session summary with the UUID of the
    session.
    
    Part of #927

--- transport_nantes/mobilito/urls.py
+++ transport_nantes/mobilito/urls.py
@@ -11,6 +11,8 @@ urlpatterns = [
 
     path('enregistrement/', views.RecordingView.as_view(), name='recording'),
     path('merci/', views.ThankYouView.as_view(), name='thanks'),
+    path('session/<uuid:uuid>/', views.SessionSummaryView.as_view(),
+         name='session_summary'),
 
     path('ajax/create-event/', views.create_event, name='event_creation'),
 ]
