7ed91db818dc127d10e3cdb219e8713715004093
Benjamin Mourgues <benjamin@mobilitains.fr>
2022-10-03 16:33:51 +0200

    Make localisation field not required in mobilito form
    Closes #939

--- transport_nantes/mobilito/forms.py
+++ transport_nantes/mobilito/forms.py
@@ -2,7 +2,8 @@ from django import forms
 
 
 class AddressForm(forms.Form):
-    location = forms.CharField(label='Localisation', max_length=1000)
+    location = forms.CharField(label='Localisation', max_length=1000,
+                               required=False)
     longitude = forms.FloatField(label='Longitude', widget=forms.HiddenInput(),
                                  required=False)
     latitude = forms.FloatField(label='Latitude', widget=forms.HiddenInput(),
