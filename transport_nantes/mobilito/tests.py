defa2c114c409a2ea3a68abe36af10d042ff9b7b
Benjamin Mourgues <benjamin@mobilitains.fr>
2022-10-05 11:28:20 +0200

    Add tests to report session feature
    Part of #928

--- transport_nantes/mobilito/tests.py
+++ transport_nantes/mobilito/tests.py
@@ -1,12 +1,11 @@
 from datetime import datetime, timezone
 
 from django.contrib.auth.models import User, Permission
-from django.test import TestCase, Client, RequestFactory
+from django.test import TestCase, Client
 from django.urls import reverse_lazy, reverse
-from http.cookies import SimpleCookie
 
-from .models import MobilitoSession, MobilitoUser
-from .views import TutorialState, TutorialView
+from .models import MobilitoSession, MobilitoUser, InappropriateFlag
+from .views import TutorialState
 
 
 class TutorialStateTests(TestCase):
@@ -16,7 +15,8 @@ class TutorialStateTests(TestCase):
 
     def test_static_state(self):
         self.assertEqual(self.tutorial_state.default_page(), "presentation")
-        self.assertEqual(self.tutorial_state.canonical_page("presentation"), "presentation")
+        self.assertEqual(
+            self.tutorial_state.canonical_page("presentation"), "presentation")
         self.assertEqual(self.tutorial_state.canonical_page("velos"), "velos")
         self.assertEqual(self.tutorial_state.canonical_page("unknown"), "presentation")
 
@@ -50,10 +50,14 @@ class MobilitoSessionViewTests(TestCase):
         self.mobilito_session_url = reverse_lazy(
             'mobilito:mobilito_session_summary',
             args=[self.mobilito_session.session_sha1])
+        self.flag_session_url = reverse_lazy(
+            'mobilito:flag_session',
+            args=[self.mobilito_session.session_sha1])
 
-        # User with permission to view unpublished sessions
+        # User with permission to view unpublished  mobilito sessions
         self.authorised_user = User.objects.create(username='bar', password='foo')
-        may_view_sessions = Permission.objects.get(codename="mobilito_session.view_session")
+        may_view_sessions = Permission.objects.get(
+            codename="mobilito_session.view_session")
         self.authorised_user.user_permissions.add(may_view_sessions)
 
         # Creating 3 clients for each user type :
@@ -74,8 +78,7 @@ class MobilitoSessionViewTests(TestCase):
             {"client": self.author_client, "code": 200,
              "msg": "MobilitoSession's author is able to see this page"},
             {"client": self.anonymous_client, "code": 404,
-             "msg": ("Users without proper permissions can't have "
-                     "access to this page.")},
+             "msg": ("Anon users can't have access to this page.")},
             {"client": self.authorised_user_client, "code": 200,
              "msg": "The page must return 200 the user has the permission."}
         ]
@@ -106,3 +109,68 @@ class MobilitoSessionViewTests(TestCase):
             response = client_code["client"].get(self.mobilito_session_url)
             self.assertEqual(
                 response.status_code, client_code["code"], client_code["msg"])
+
+    def test_flag_session(self):
+        """Test the ability flag a session."""
+
+        # GET method is not allowed
+        for clients in self.no_perm_needed_responses:
+            response = clients["client"].get(self.flag_session_url)
+            self.assertEqual(
+                response.status_code, 405, "GET method is not allowed.")
+
+        # POST method is allowed for authenticated users
+        post_data = {
+            "report-abuse-text": "This is a test report.",
+        }
+        response = self.anonymous_client.post(self.flag_session_url, post_data)
+        self.assertEqual(
+            response.status_code, 200,
+            "POST method is allowed for anonymous users")
+        self.assertEqual(InappropriateFlag.objects.count(),
+                         1, "An InappropriateFlag must be created")
+
+        response = self.authorised_user_client.post(
+            self.flag_session_url, post_data)
+        self.assertEqual(
+            response.status_code, 200,
+            "POST method is allowed for authorised users.")
+        self.assertEqual(InappropriateFlag.objects.count(),
+                         2, "An InappropriateFlag must be created.")
+
+        # Trying to flag a session twice doesn't create a new record.
+        response = self.authorised_user_client.post(
+            self.flag_session_url, post_data)
+        self.assertEqual(
+            response.status_code, 200,
+            "POST method is allowed for authorised users.")
+        self.assertEqual(InappropriateFlag.objects.count(), 2,
+                         "User may flag only once for a given session")
+
+        response = self.anonymous_client.post(
+            self.flag_session_url, post_data)
+        self.assertEqual(
+            response.status_code, 200,
+            "POST method is allowed for anonymous users")
+        self.assertEqual(InappropriateFlag.objects.count(), 2,
+                         "Anonymous users may flag only once for "
+                         "a given mobilito session")
+
+        # Trying to flag a session that doesn't exist returns a 404
+        response = self.authorised_user_client.post(
+            reverse_lazy('mobilito:flag_session', args=["foo"]), post_data)
+        self.assertEqual(
+            response.status_code, 404,
+            "Users can't flag a session that doesn't exist.")
+        self.assertEqual(InappropriateFlag.objects.count(), 2,
+                         ("No new report should be created when there isn't a"
+                         " matching MobilitoSession."))
+
+        response = self.anonymous_client.post(
+            reverse_lazy('mobilito:flag_session', args=["foo"]), post_data)
+        self.assertEqual(
+            response.status_code, 404,
+            "Anonymous users can't flag a Mobilito Session that doesn't exist.")
+        self.assertEqual(InappropriateFlag.objects.count(), 2,
+                         ("No new report should be created when there isn't a"
+                          " matching mobilitoSession."))
