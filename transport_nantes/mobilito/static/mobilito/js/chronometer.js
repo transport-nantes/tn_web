243383870c1c15fe082cb0a34ccdc359b8d35838
Benjamin Mourgues <benjamin@mobilitains.fr>
2022-11-23 12:50:58 +0100

    MVP -- Multi level progress bar concept
    The bar would change color progressively, we could add some texts
    to cheer users that reach certain thresholds.
    
    Part of #1006

--- transport_nantes/mobilito/static/mobilito/js/chronometer.js
+++ transport_nantes/mobilito/static/mobilito/js/chronometer.js
@@ -3,6 +3,45 @@ let minutes = 0;
 let hours = 0;
 
 var chronometer = document.getElementById('chrono');
+var progressBar = document.getElementById('bar');
+
+var completedBarTime = 0;
+
+var FirstStage = {
+    duration: 5,
+    styles: () =>{return;}
+}
+var SecondStage = {
+    duration: 3,
+    styles: () =>{
+        $(".progress")[0].classList.add("bg-success");
+        progressBar.classList.remove("bg-success");
+        progressBar.classList.add("bg-warning");
+    }
+}
+var ThirdStage = {
+    duration: 4,
+    styles: () =>{
+        $(".progress")[0].classList.remove("bg-success");
+        $(".progress")[0].classList.add("bg-warning");
+        progressBar.classList.remove("bg-warning");
+        progressBar.classList.add("bg-danger");
+    }
+}
+
+var stages = [FirstStage, SecondStage, ThirdStage];
+function* stagesIterator(stages) {
+    for (let i = 0; i < stages.length; i++) {
+        yield stages[i];
+    }
+}
+
+var stage = stagesIterator(stages);
+var currentStage = stage.next().value;
+var goal = currentStage.duration;
+var applyStyles = () => {
+    currentStage.styles();
+}
 
 const updateTime = () => {
     seconds = parseInt(seconds);
@@ -10,7 +49,8 @@ const updateTime = () => {
     hours = parseInt(hours);
 
     seconds++;
-    
+    var total_seconds = seconds + (minutes * 60) + (hours * 3600);
+
     if (seconds == 60) {
         minutes++;
         seconds = 0;
@@ -33,6 +73,27 @@ const updateTime = () => {
         hours = "0" + hours;
     }
     chronometer.innerHTML = `${hours}:${minutes}:${seconds}`;
+    updateProgressBar(total_seconds);
+}
+
+const updateProgressBar = (total_seconds) => {
+
+    total_seconds -= completedBarTime;
+    var progress = (total_seconds / goal) * 100;
+
+    progressBar.style.width = `${progress}%`;
+    applyStyles();
+
+    if (progress >= 100) {
+        if (currentStage) {
+            completedBarTime += currentStage.duration;
+        }
+        currentStage = stage.next().value;
+        if (currentStage) {
+            goal = currentStage.duration
+            applyStyles = currentStage.styles;
+        }
+    }
 }
 
 setInterval(updateTime, 1000);
