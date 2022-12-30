e8ec41926f8594615696e89d6d641ab657e5973f
Jeff Abrahamson <jeff@p27.eu>
2022-12-30 08:22:06 +0100

    Fix photo gallery back/forward buttons
    The query we used to use for moving backwards and forwards led to
    unexpected cycles that didn't cover the entire set of images.
    Verifying the correctness of the queries, however, was more than a bit
    difficult.
    
    This commit simplifies the query logic so that it is easily
    inspectable.  It also changes sort order to sha1_name instead of
    primary key (no sense publishing photo arrival order) and guarantees
    that the gallery will display photos in the same order as the
    carousel.
    
    This commit also changes the display logic: instead of computing
    previous and next image links in advance, we lazily compute them on
    request.  This permits simplifying the display-one-image logic.  The
    database load, not to mention its change, is not significant at our
    scale, so I've not even tried to measure it.
    
    Improve tests for photo display.
    
    Part of #1096.

--- transport_nantes/photo/urls.py
+++ transport_nantes/photo/urls.py
@@ -10,13 +10,24 @@ database-driven concepts of which competition and so forth.
 
 """
 
-app_name = 'photo'
+app_name = "photo"
 urlpatterns = [
-    path('upload/', views.UploadEntry.as_view(),
-         name='upload'),
-    path('confirmation/', views.Confirmation.as_view(),
-         name='confirmation'),
+    path("upload/", views.UploadEntry.as_view(), name="upload"),
+    path("confirmation/", views.Confirmation.as_view(), name="confirmation"),
     path("galerie/", views.PhotoListView.as_view(), name="galerie"),
-    path('galerie/<str:photo_sha1>', views.PhotoView.as_view(),
-         name='photo_details'),
+    path(
+        "galerie/<str:photo_sha1>",
+        views.PhotoView.as_view(),
+        name="photo_details",
+    ),
+    path(
+        "galerie/n/<str:photo_sha1>",
+        views.NextPhotoView.as_view(),
+        name="next_photo_details",
+    ),
+    path(
+        "galerie/p/<str:photo_sha1>",
+        views.PrevPhotoView.as_view(),
+        name="prev_photo_details",
+    ),
 ]
