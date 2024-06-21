11b6f036834b723c7facdfdbd787cf8f60a70d1e
Jeff Abrahamson <jeff@p27.eu>
2024-06-21 15:23:20 +0200

    Improve survey admin interface

--- transport_nantes/surveys/admin.py
+++ transport_nantes/surveys/admin.py
@@ -11,6 +11,52 @@ from .models import (
 
 admin.site.register(Survey)
 admin.site.register(SurveyCommune)
-admin.site.register(SurveyQuestion)
-admin.site.register(SurveyResponder)
-admin.site.register(SurveyResponse)
+
+
+class SurveyQuestionAdmin(admin.ModelAdmin):
+    list_display = (
+        "survey",
+        "question_title",
+    )
+    search_fields = (
+        "survey__name",
+        "question_title",
+    )
+
+
+admin.site.register(SurveyQuestion, SurveyQuestionAdmin)
+
+
+class SurveyResponderAdmin(admin.ModelAdmin):
+    list_display = ("liste", "tete_de_liste", "commune", "survey")
+    list_filter = (
+        "commune__commune",
+        "survey__name",
+    )
+    search_fields = (
+        "commune__commune",
+        "survey__name",
+    )
+
+
+admin.site.register(SurveyResponder, SurveyResponderAdmin)
+
+
+class SurveyResponseAdmin(admin.ModelAdmin):
+    list_display = (
+        "survey_responder",
+        "survey_question",
+    )
+    list_filter = (
+        "survey_responder__commune__commune",
+        "survey_responder__survey__name",
+        "survey_question__question_title",
+    )
+    search_fields = (
+        "survey_responder__commune__commune",
+        "survey_responder__survey__name",
+        "survey_question__question_title",
+    )
+
+
+admin.site.register(SurveyResponse, SurveyResponseAdmin)
